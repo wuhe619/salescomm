ce9da0e716c406e5df523c20fd07c1867f5b859b
zhangbf <10369446@qq.com>
2019-08-19 19:44:36 +0800

    加上销售定价和余额的判断验证 去掉注释

--- src/main/java/com/bdaim/batch/service/impl/ExpressBatchServiceImpl.java
+++ src/main/java/com/bdaim/batch/service/impl/ExpressBatchServiceImpl.java
@@ -111,24 +111,24 @@ public class ExpressBatchServiceImpl implements ExpressBatchService {
             return new ResponseInfoAssemble().failure(HttpStatus.BAD_REQUEST.value(), "请保证一次上传数据不超过1000条记录");
         }
         //2.5 加上销售定价判断和余额的判断
-//        String priceSql = "SELECT cust_id,property_value FROM t_customer_property WHERE cust_id='" + custId + "' AND property_name='price'";
-//        List<Map<String, Object>> priceList = jdbcTemplate.queryForList(priceSql);
-//        if (priceList == null || priceList.size() == 0) {
-//            return new ResponseInfoAssemble().failure(HttpStatus.BAD_REQUEST.value(), "请先设置销售定价");
-//        } else {
-//            // 判断余额是否足够 t_customer_property中的price的value是销售定价，单位 元，remain_amount是余额，单位分
-//            int num = contentList.size() - 1;
-//            String remainAmountSql = "SELECT cust_id,property_value FROM t_customer_property WHERE cust_id='" + custId + "' AND property_name='remain_amount'";
-//            Map<String, Object> amountMap = jdbcTemplate.queryForMap(remainAmountSql);
-//            if (amountMap != null) {
-//                int price = Integer.parseInt(String.valueOf(priceList.get(0).get("property_value")));
-//                int remainAmount = Integer.parseInt(String.valueOf(amountMap.get("property_value")));
-//                int remain = remainAmount - price * num * 100;
-//                if (remain < 0) {
-//                    return new ResponseInfoAssemble().failure(HttpStatus.BAD_REQUEST.value(), "余额不足，请先充值");
-//                }
-//            }
-//        }
+        String priceSql = "SELECT cust_id,property_value FROM t_customer_property WHERE cust_id='" + custId + "' AND property_name='price'";
+        List<Map<String, Object>> priceList = jdbcTemplate.queryForList(priceSql);
+        if (priceList == null || priceList.size() == 0) {
+            return new ResponseInfoAssemble().failure(HttpStatus.BAD_REQUEST.value(), "请先设置销售定价");
+        } else {
+            // 判断余额是否足够 t_customer_property中的price的value是销售定价，单位 元，remain_amount是余额，单位分
+            int num = contentList.size() - 1;
+            String remainAmountSql = "SELECT cust_id,property_value FROM t_customer_property WHERE cust_id='" + custId + "' AND property_name='remain_amount'";
+            Map<String, Object> amountMap = jdbcTemplate.queryForMap(remainAmountSql);
+            if (amountMap != null) {
+                int price = Integer.parseInt(String.valueOf(priceList.get(0).get("property_value")));
+                int remainAmount = Integer.parseInt(String.valueOf(amountMap.get("property_value")));
+                int remain = remainAmount - price * num * 100;
+                if (remain < 0) {
+                    return new ResponseInfoAssemble().failure(HttpStatus.BAD_REQUEST.value(), "余额不足，请先充值");
+                }
+            }
+        }
         //3. 把批次信息和批次详情列表存入数据库
         //3.1 设置并新增保存批次信息 (status 为"1",表示 【校验中】) (certify_type是修复方式 3.表示快递地址修复)
         StringBuffer insertBatchSql = new StringBuffer("INSERT INTO nl_batch (id,batch_name,upload_time,comp_id,certify_type,channel,status," +
