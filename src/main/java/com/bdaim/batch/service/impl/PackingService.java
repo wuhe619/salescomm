9e4cc6a37bfcdd2d6b1a35814a57d696a75bdcb9
zhangbf <10369446@qq.com>
2019-08-20 14:21:04 +0800

    发送快递 测试版

--- src/main/java/com/bdaim/batch/service/impl/PackingService.java
+++ src/main/java/com/bdaim/batch/service/impl/PackingService.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.bdaim.batch.dao.BatchDetailDao;
 import com.bdaim.batch.entity.BatchDetail;
+import com.bdaim.batch.express.ZopClient;
+import com.bdaim.batch.express.ZopPublicRequest;
 import com.bdaim.common.util.ConstantsUtil;
 import com.bdaim.common.util.IDHelper;
 import com.bdaim.common.util.StringUtil;
@@ -26,8 +28,11 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -56,7 +61,6 @@ public class PackingService {
         List<Map<String, Object>> list = batchDetailDao.sqlQuery(querySql);
         return list;
     }
-
 
 
     public List<Map<String, Object>> getImeiResoult() {
@@ -104,7 +108,7 @@ public class PackingService {
      * @date 2019/8/8 14:42
      */
     public void sendExpress(Map<String, Object> map) {
-        try{
+        try {
             //map中包括批次ID和地址ID，以及isBatch状态判断，
             int isBatch = Integer.parseInt(String.valueOf(map.get("isBatch")));
             String batchId = String.valueOf(map.get("batchId"));
@@ -112,7 +116,7 @@ public class PackingService {
             //发件人ID
             String senderId = String.valueOf(map.get("senderId"));
             String updateBatchStatus = "UPDATE nl_batch SET status='5' WHERE id='" + batchId + "'";
-            logger.info("执行更新状态的SQL :"+updateBatchStatus);
+            logger.info("执行更新状态的SQL :" + updateBatchStatus);
             if (isBatch == 1) {
                 //批量发送、将批次状态status修改为【5】【待取件】
                 jdbcTemplate.update(updateBatchStatus);
@@ -134,8 +138,8 @@ public class PackingService {
                 }
                 toSendExpress(isBatch, batchId, addressId, senderId);
             }
-        }catch (Exception e){
-            logger.info("发送快递出错，异常信息为"+e.getMessage());
+        } catch (Exception e) {
+            logger.info("发送快递出错，异常信息为" + e.getMessage());
         }
     }
 
@@ -157,20 +161,73 @@ public class PackingService {
         Map<String, Object> senderInfo = jdbcTemplate.queryForMap(senderSql);
         if (isBatch == 1) {
             //批量发送，根据批次ID batchId 找出地址ID、姓名、手机号
-            StringBuffer stringBuffer = new StringBuffer("SELECT id AS addressId,label_one AS name,label_two AS phone,label_eight AS pdfPath FROM nl_batch_detail WHERE batch_id='");
+            StringBuffer stringBuffer = new StringBuffer("SELECT id AS addressId,touch_id,label_one AS name,label_two AS phone,label_eight AS pdfPath FROM nl_batch_detail WHERE batch_id='");
             stringBuffer.append(batchId).append("' AND status='1'");
             List<Map<String, Object>> resultList = jdbcTemplate.queryForList(stringBuffer.toString());
             for (Map<String, Object> tempMap : resultList) {
                 updateExpressInfo(tempMap, senderInfo);
-
+                sendExpressByZTO(batchId, addressId, senderInfo, tempMap);
             }
         } else if (isBatch == 0) {
             //单个发送，根据地址ID 找到 地址ID、姓名、手机号
-            StringBuffer stringBuffer = new StringBuffer("SELECT id AS addressId,label_one AS name,label_two AS phone,label_eight AS pdfPath FROM nl_batch_detail WHERE id='");
+            StringBuffer stringBuffer = new StringBuffer("SELECT id AS addressId,touch_id,label_one AS name,label_two AS phone,label_eight AS pdfPath FROM nl_batch_detail WHERE id='");
             stringBuffer.append(addressId).append("'");
             Map<String, Object> tempMap = jdbcTemplate.queryForMap(stringBuffer.toString());
             updateExpressInfo(tempMap, senderInfo);
+            //调用发送快递的接口
+            sendExpressByZTO(batchId, addressId, senderInfo, tempMap);
         }
+    }
+
+    /**
+     * 调用中通快递的接口
+     * 1. 创建快递订单【API文档中的订单服务 -> 预约寄件 -订单创建】
+     *
+     * @param batchId      批次编号
+     * @param addressId    修复地址ID
+     * @param senderInfo   发件人信息
+     * @param receiverInfo 收件人信息
+     */
+    private void sendExpressByZTO(String batchId, String addressId, Map<String, Object> senderInfo, Map<String, Object> receiverInfo) {
+        logger.info(" ===== 》》》开始调用中通快递接口");
+        logger.info("批次编号" + batchId + "修复地址ID" + addressId + "发件人信息" + senderInfo + "收件人信息" + receiverInfo);
+        ZopClient client = new ZopClient("ztoOrderTest", "enRvMTIzc2lnbndoeA==");
+        ZopPublicRequest request = new ZopPublicRequest();
+        request.setUrl("http://58.40.16.122:8080/exposeServicePushOrderService");
+        request.addParam("company_id", "ztoOrderTest");
+        Map<String, Object> data = new HashMap<>(32);
+        data.put("orderId", receiverInfo.get("touch_id"));
+        //TODO （正式的shopKey请发送邮件至kdgj1@zto.cn申请）
+        data.put("shopKey", "T3BlbktER0pfMjAxNzUxNzEyMjAyODI=");
+        //订单类型 0代表普通订单 1代表代收货款
+        data.put("orderType", "0");
+        //收件人信息
+        data.put("receiveMan", receiverInfo.get("name"));
+        data.put("receivePhone", receiverInfo.get("phone"));
+        //TODO 收件人的省市区 地址 向吴哥和立莹要
+        data.put("receiveProvince", "收件人省");
+        data.put("receiveCity", "市");
+        data.put("receiveCounty", "区县");
+        data.put("receiveAddress", "详细地址");
+        //发件人信息
+        data.put("sendMan", senderInfo.get("senderName"));
+        data.put("sendMobile", senderInfo.get("phone"));
+        data.put("sendProvince", senderInfo.get("province"));
+        data.put("sendCity", senderInfo.get("city"));
+        data.put("sendCounty", senderInfo.get("district"));
+        data.put("sendAddress", senderInfo.get("address"));
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        data.put("orderDate", format.format(new Date()));
+        request.addParam("data", JSON.toJSONString(data));
+        try {
+            logger.info("订单创建成功，返回值为");
+            logger.info(client.execute(request));
+        } catch (Exception e) {
+            logger.info("订单创建失败，返回值为");
+            logger.info(e.getMessage());
+        }
+
+
     }
 
     /**
