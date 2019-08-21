7eacff2330d03b606d450a0f30bdaac1e9106a85
zhangbf <10369446@qq.com>
2019-08-21 15:32:27 +0800

    时间格式 修改

--- src/main/java/com/bdaim/bill/service/impl/BillServiceImpl.java
+++ src/main/java/com/bdaim/bill/service/impl/BillServiceImpl.java
@@ -1349,7 +1349,7 @@ public class BillServiceImpl implements BillService {
     }
 
     public Page getListCustomerBill(CustomerBillQueryParam param) throws Exception {
-        StringBuffer querySql = new StringBuffer("SELECT n.comp_id custId,b.batch_id batchId,n.batch_name batchName,n.upload_time uploadTime,IFNULL(SUM(b.amount) / 100,0) amount ,IFNULL(SUM(b.prod_amount) / 100,0) prodAmount , ");
+        StringBuffer querySql = new StringBuffer("SELECT n.comp_id custId,b.batch_id batchId,n.batch_name batchName,DATE_FORMAT(n.upload_time,'%Y-%m-%d %H:%i:%s') AS uploadTime,IFNULL(SUM(b.amount) / 100,0) amount ,IFNULL(SUM(b.prod_amount) / 100,0) prodAmount , ");
         querySql.append("( SELECT COUNT(id) FROM nl_batch_detail WHERE batch_id = b.batch_id ) fixNumber ");
         querySql.append("FROM stat_bill_month b LEFT JOIN nl_batch n ON b.batch_id = n.id  LEFT JOIN t_customer c ON  n.comp_id = c.cust_id ");
         querySql.append("WHERE n.certify_type = 3 ");
