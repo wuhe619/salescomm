1ccdc32423f410c64ad0a65f6e4e1c98e035c820
duanly <dly123456>
2019-08-22 19:46:46 +0800

    修改企业账单地址拼接

--- src/main/java/com/bdaim/bill/service/impl/BillServiceImpl.java
+++ src/main/java/com/bdaim/bill/service/impl/BillServiceImpl.java
@@ -1457,7 +1457,7 @@ public class BillServiceImpl implements BillService {
      * @return
      */
     public Page getBillDetailList(CustomerBillQueryParam param) {
-        StringBuffer querySql = new StringBuffer("SELECT d.batch_id batchId, d.site address,l.touch_id expressId,l.resource_id expressResource, d.resource_id fixResource,l.create_time sendTime,d.label_one name,d.label_two phone, d.label_five peopleId, IFNULL(d.amount / 100, 0) amount, IFNULL(d.prod_amount / 100, 0) prodAmount, IFNULL(l.amount / 100, 0) expressAmount, IFNULL(l.prod_amount / 100, 0) exProdAmount,");
+        StringBuffer querySql = new StringBuffer("SELECT d.batch_id batchId, DATE_FORMAT(d.fix_time ,'%Y-%m-%d %H:%i:%s') AS fixTime,d.site address,l.touch_id expressId,l.resource_id expressResource, d.resource_id fixResource,l.create_time sendTime,d.label_one name,d.label_two phone, d.label_five peopleId, IFNULL(d.amount / 100, 0) amount, IFNULL(d.prod_amount / 100, 0) prodAmount, IFNULL(l.amount / 100, 0) expressAmount, IFNULL(l.prod_amount / 100, 0) exProdAmount,");
         querySql.append("(SELECT s.`name` FROM t_market_resource r LEFT JOIN t_supplier s ON s.supplier_id = r.supplier_id WHERE r.resource_id = l.resource_id) expressSupplier,");
         querySql.append("(SELECT s.`name` FROM t_market_resource r LEFT JOIN t_supplier s ON s.supplier_id = r.supplier_id WHERE r.resource_id = d.resource_id) fixSupplier");
         querySql.append(" FROM nl_batch_detail d LEFT JOIN t_touch_express_log l ON d.touch_id = l.touch_id ");
@@ -1491,6 +1491,20 @@ public class BillServiceImpl implements BillService {
                 if (StringUtil.isNotEmpty(String.valueOf(list.get(i).get("amount"))) && StringUtil.isNotEmpty(String.valueOf(list.get(i).get("prodAmount")))) {
                     String profit = new BigDecimal(String.valueOf(list.get(i).get("amount"))).subtract(new BigDecimal(String.valueOf(list.get(i).get("prodAmount")))).setScale(2, BigDecimal.ROUND_DOWN).toString();
                     list.get(i).put("profit", profit);
+                }
+                //拼接地址展示结构
+                if (StringUtil.isNotEmpty(String.valueOf(list.get(i).get("address")))) {
+                    JSONObject jsonObject = JSON.parseObject(String.valueOf(list.get(i).get("address")));
+                    String prov = jsonObject.getString("prov");
+                    String city = jsonObject.getString("city");
+                    String address ="";
+                    if (StringUtil.isNotEmpty(prov)){
+                        address+=prov;
+                    }
+                    if (StringUtil.isNotEmpty(city)){
+                        address+=city;
+                    }
+                    list.get(i).put("address",address);
                 }
             }
         }
