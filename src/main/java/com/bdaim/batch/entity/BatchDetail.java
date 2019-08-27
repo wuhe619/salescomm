ae8c7b8f8a8f66f76f3684e3fa622685a993e665
zhangbf <10369446@qq.com>
2019-08-27 10:10:02 +0800

    去掉phoneId

--- src/main/java/com/bdaim/batch/entity/BatchDetail.java
+++ src/main/java/com/bdaim/batch/entity/BatchDetail.java
@@ -340,15 +340,15 @@ public class BatchDetail implements Serializable {
     public void setPhoneNum(String phoneNum) {
         this.phoneNum = phoneNum;
     }*/
-    @Basic
-    @Column(name = "phoneId")
-    public String getPhoneId() {
-        return phoneId;
-    }
-
-    public void setPhoneId(String phoneId) {
-        this.phoneId = phoneId;
-    }
+//    @Basic
+//    @Column(name = "phoneId")
+//    public String getPhoneId() {
+//        return phoneId;
+//    }
+//
+//    public void setPhoneId(String phoneId) {
+//        this.phoneId = phoneId;
+//    }
 
     @Basic
     @Column(name = "site")
@@ -443,7 +443,7 @@ public class BatchDetail implements Serializable {
                 ", lastCallTime=" + lastCallTime +
                 ", allocation=" + allocation +
                 ", provideId='" + provideId + '\'' +
-                ", phoneId='" + phoneId + '\'' +
+//                ", phoneId='" + phoneId + '\'' +
                 ", site='" + site + '\'' +
                 ", name='" + name + '\'' +
                 ", activityId='" + activityId + '\'' +
