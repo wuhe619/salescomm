0dd8ff2161a277c381bc8ad8d52f41356f48c7d3
chengning <chengning@salescomm.net>
2019-09-09 20:42:27 +0800

    feat:默认返回体Content-Type

--- src/main/java/com/bdaim/common/PathConfig.java
+++ src/main/java/com/bdaim/common/PathConfig.java
@@ -1,7 +1,10 @@
 package com.bdaim.common;
 
+import com.bdaim.common.util.PropertiesUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,6 +22,8 @@ import java.util.Properties;
 public class PathConfig implements WebMvcConfigurer {
     protected static final Properties PROPERTIES = new Properties(System.getProperties());
 
+    private static final String APP_NAME = PropertiesUtil.getStringValue("app");
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String pathF = PROPERTIES.getProperty("file.separator");
@@ -29,9 +34,17 @@ public class PathConfig implements WebMvcConfigurer {
     public AuthInterceptor securityInterceptor() {
         return new AuthInterceptor();
     }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 添加拦截器
         registry.addInterceptor(securityInterceptor()).addPathPatterns("/open/**");
+    }
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        // 标识不为精准营销项目则不进行返回值操作
+        if ("online".equalsIgnoreCase(APP_NAME)) {
+            configurer.defaultContentType(MediaType.APPLICATION_JSON);
+        }
     }
 }
