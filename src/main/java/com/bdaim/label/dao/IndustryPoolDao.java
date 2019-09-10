0616eb8593b0befacddd51a11b62bd53893cca61
chengning <chengning@salescomm.net>
2019-09-10 14:18:03 +0800

    feat:SupplierEntity hql修改

--- src/main/java/com/bdaim/label/dao/IndustryPoolDao.java
+++ src/main/java/com/bdaim/label/dao/IndustryPoolDao.java
@@ -1,6 +1,7 @@
 package com.bdaim.label.dao;
 
 import com.bdaim.common.dao.SimpleHibernateDao;
+import com.bdaim.common.util.NumberConvertUtil;
 import com.bdaim.label.entity.IndustryPool;
 import com.bdaim.resource.entity.MarketResourceEntity;
 import com.bdaim.supplier.dto.SupplierDTO;
@@ -18,9 +19,9 @@ public class IndustryPoolDao extends SimpleHibernateDao<IndustryPool, Serializab
 
     public SupplierDTO getSupplierInfo(int poolId) {
         IndustryPool industryPool = (IndustryPool) this.get(poolId);
-        MarketResourceEntity marketResource = this.findUnique(" FROM MarketResourceEntity m where m.resourceId=?", (long) industryPool.getSourceId());
+        MarketResourceEntity marketResource = this.findUnique(" FROM MarketResourceEntity m where m.resourceId=?", industryPool.getSourceId());
         if (marketResource != null) {
-            SupplierEntity supplierDO = this.findUnique(" FROM SupplierDO m where m.supplierId=?", Long.parseLong(marketResource.getSupplierId()));
+            SupplierEntity supplierDO = this.findUnique(" FROM SupplierEntity m where m.supplierId=?", NumberConvertUtil.parseInt(marketResource.getSupplierId()));
             return new SupplierDTO(supplierDO);
         }
         return null;
