7a1e077d450f3f1b6c87f5946a50bc4e3a5a0e63
wuhe <wuhe@bdaim.com>
2019-09-21 17:51:30 +0800

    申报单分单添加商品

--- src/main/java/com/bdaim/customs/services/SbdSService.java
+++ src/main/java/com/bdaim/customs/services/SbdSService.java
@@ -1,10 +1,25 @@
 package com.bdaim.customs.services;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.bdaim.common.dto.Page;
 import com.bdaim.common.service.BusiService;
+import com.bdaim.common.service.ElasticSearchService;
+import com.bdaim.common.service.ResourceService;
+import com.bdaim.common.util.StringUtil;
+import com.bdaim.customs.dao.HBusiDataManagerDao;
+import com.bdaim.customs.entity.BusiTypeEnum;
+import com.bdaim.customs.entity.Constants;
+import com.bdaim.customs.entity.HBusiDataManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /***
  * 申报单.税单
@@ -12,10 +27,116 @@ import java.util.List;
 @Service("busi_sbd_s")
 public class SbdSService implements BusiService {
 
-    @Override
-    public void insertInfo(String busiType, String cust_id, String cust_group_id, Long cust_user_id, Long id, JSONObject info) {
-        // TODO Auto-generated method stub
+    private static Logger log = LoggerFactory.getLogger(SbdFService.class);
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private HBusiDataManagerDao hBusiDataManagerDao;
+
+    @Autowired
+    private ElasticSearchService elasticSearchService;
+
+    @Autowired
+    private ResourceService resourceService;
+
+
+    @Override
+    public void insertInfo(String busiType, String cust_id, String cust_group_id, Long cust_user_id, Long id, JSONObject info) throws Exception {
+        Integer pid = info.getInteger("pid");
+        String billNo = info.getString("party_no");
+        if(pid==null){
+            log.error("分单id不能为空");
+            throw new Exception("分单id不能为空");
+        }
+        if(StringUtil.isEmpty(billNo)){
+            log.error("分单号不能为空");
+            throw new Exception("分单号不能为空");
+        }
+        String code_ts=info.getString("code_ts");
+        if(StringUtil.isEmpty(code_ts)){
+            log.error("商品编码不能为空");
+            throw new Exception("商品编码不能为空");
+        }
+        info.put("cust_id",cust_id);
+        info.put("create_id",cust_user_id);
+        info.put("create_date",new Date());
+        info.put("ext_3",code_ts);
+        info.put("ext_4",billNo);
+        float duty_paid_price = 0;
+        float estimated_tax = 0;
+        float tax_rate = 0;
+        int is_low_price = 0;
+
+        if(StringUtil.isNotEmpty(code_ts)) {
+            JSONObject params = new JSONObject();
+            params.put("code", code_ts);
+            Page page = resourceService.query("", "duty_paid_rate",params);
+            if(page!=null && page.getTotal()>0){
+                List dataList = page.getData();
+                Map<String ,Object> d = (Map<String, Object>) dataList.get(0);
+                String content = (String) d.get("content");
+                JSONObject contentObj=JSON.parseObject(content);
+                duty_paid_price = contentObj.getFloatValue("duty_price");
+                if(StringUtil.isNotEmpty(info.getString("decl_price"))){
+                    if(Float.valueOf(info.getString("decl_price")) < duty_paid_price){
+                        is_low_price = 1;
+                    }
+                }
+                tax_rate = contentObj.getFloatValue("tax_rate");
+                estimated_tax = duty_paid_price*tax_rate;
+            }
+        }
+        info.put("is_low_price",is_low_price);
+        info.put("duty_paid_price", duty_paid_price);//完税价格
+        info.put("estimated_tax", estimated_tax);//预估税金
+        info.put("tax_rate", tax_rate);//税率
+        info.put("total_price",0);//价格合计
+
+        addDataToES(id.toString(),busiType,info);
+
+
+        HBusiDataManager partH = getObjectByIdAndType(pid.longValue(),BusiTypeEnum.SF.getType());
+
+        String pcontent = partH.getContent();
+        JSONObject jsonObject = JSON.parseObject(pcontent);
+        Float weight = jsonObject.getFloatValue("weight");
+        Float pack_NO = jsonObject.getFloatValue("pack_no");
+        if (weight == null) weight = 0f;
+        if (info.containsKey("ggrosswt") && StringUtil.isNotEmpty(info.getString("ggrosswt"))) {
+            weight += Float.valueOf(info.getString("ggrosswt"));
+        }
+        if (pack_NO == null) pack_NO = 0f;
+        if (info.containsKey("g_qty") && StringUtil.isNotEmpty(info.getString("g_qty"))) {
+            pack_NO += Float.valueOf(info.getString("g_qty"));
+        }
+        jsonObject.put("weight", weight);
+        jsonObject.put("pack_no", pack_NO);
+        Integer lowPricegoods = jsonObject.getInteger("low_price_goods");
+        if(lowPricegoods==null)lowPricegoods=0;
+        jsonObject.put("low_price_goods",lowPricegoods+is_low_price);
+        partH.setContent(jsonObject.toJSONString());
+        hBusiDataManagerDao.saveOrUpdate(partH);
+
+        updateDataToES(BusiTypeEnum.SF.getType(),pid.toString(),jsonObject);
+
+        HBusiDataManager zh = getObjectByIdAndType(jsonObject.getLong("pid"), BusiTypeEnum.SZ.getKey());
+        String zcontent = zh.getContent();
+        JSONObject jsonz = JSON.parseObject(zcontent);
+        Float weight_total = jsonz.getFloatValue("weight_total");
+        Integer lowPricegoodsz = jsonObject.getInteger("low_price_goods");
+        if(lowPricegoodsz==null)lowPricegoodsz = 0;
+        jsonz.put("low_price_goods",lowPricegoodsz + is_low_price);
+
+        if (weight_total == null) weight_total = 0f;
+        if(StringUtil.isNotEmpty(info.getString("ggrosswt"))){
+            weight_total+=Float.valueOf(info.getString("ggrosswt"));
+        }
+        jsonz.put("weight_total", weight_total);
+
+        zh.setContent(jsonz.toJSONString());
+        hBusiDataManagerDao.saveOrUpdate(zh);
+        updateDataToES(BusiTypeEnum.SZ.getType(),zh.getId().toString(),jsonz);
     }
 
     @Override
@@ -79,6 +200,68 @@ public class SbdSService implements BusiService {
     @Override
     public void formatInfo(String busiType, String cust_id, String cust_group_id, Long cust_user_id, JSONObject info) {
         // TODO Auto-generated method stub
+
+    }
+
+    public HBusiDataManager getObjectByIdAndType(Long id,String type){
+        String sql="select * from h_data_manager where id="+id+" and type='"+type+"'";
+        return jdbcTemplate.queryForObject(sql,HBusiDataManager.class);
+    }
+
+    /**
+     * 添加数据到es
+     *
+     * @param
+     * @param
+     */
+    private void addDataToES(String id,String type,JSONObject content) {
+        if (type.equals(BusiTypeEnum.SZ.getType())) {
+            elasticSearchService.addDocumentToType(Constants.SZ_INFO_INDEX, "haiguan", id, content);
+        }else if(type.equals(BusiTypeEnum.CZ.getType())){
+            elasticSearchService.addDocumentToType(Constants.CZ_INFO_INDEX, "haiguan", id, content);
+        }else if(type.equals(BusiTypeEnum.BZ.getType())){
+            elasticSearchService.addDocumentToType(Constants.BZ_INFO_INDEX, "haiguan", id, content);
+        } else if (type.equals(BusiTypeEnum.SF.getType())) {
+            elasticSearchService.addDocumentToType(Constants.SF_INFO_INDEX, "haiguan", id, content);
+        }else if( type.equals(BusiTypeEnum.CF.getType())){
+            elasticSearchService.addDocumentToType(Constants.CF_INFO_INDEX, "haiguan", id, content);
+        }else if(type.equals(BusiTypeEnum.BF.getType())){
+            elasticSearchService.addDocumentToType(Constants.BF_INFO_INDEX, "haiguan", id,content);
+        }else if (type.equals(BusiTypeEnum.SS.getType())) {
+            elasticSearchService.addDocumentToType(Constants.SS_INFO_INDEX, "haiguan", id, content);
+        }else if(type.equals(BusiTypeEnum.CS.getType())){
+            elasticSearchService.addDocumentToType(Constants.CS_INFO_INDEX, "haiguan", id, content);
+        }else if(type.equals(BusiTypeEnum.BS.getType())){
+            elasticSearchService.addDocumentToType(Constants.BS_INFO_INDEX, "haiguan", id, content);
+        }
+    }
+
+    /**
+     * 更新es
+     * @param type
+     * @param id
+     * @param content
+     */
+    private void updateDataToES(String type,String id,JSONObject content) {
+        if (type.equals(BusiTypeEnum.SZ.getType())) {
+            elasticSearchService.updateDocumentToType(Constants.SZ_INFO_INDEX, "haiguan", id, content);
+        }else if(type.equals(BusiTypeEnum.CZ.getType())){
+            elasticSearchService.updateDocumentToType(Constants.CZ_INFO_INDEX, "haiguan", id, content);
+        }else if(type.equals(BusiTypeEnum.BZ.getType())){
+            elasticSearchService.updateDocumentToType(Constants.BZ_INFO_INDEX, "haiguan", id, content);
+        } else if (type.equals(BusiTypeEnum.SF.getType())) {
+            elasticSearchService.updateDocumentToType(Constants.SF_INFO_INDEX, "haiguan", id, content);
+        }else if( type.equals(BusiTypeEnum.CF.getType())){
+            elasticSearchService.updateDocumentToType(Constants.CF_INFO_INDEX, "haiguan", id, content);
+        }else if(type.equals(BusiTypeEnum.BF.getType())){
+            elasticSearchService.updateDocumentToType(Constants.BF_INFO_INDEX, "haiguan", id, content);
+        }else if (type.equals(BusiTypeEnum.SS.getType())) {
+            elasticSearchService.updateDocumentToType(Constants.SS_INFO_INDEX, "haiguan", id, content);
+        }else if(type.equals(BusiTypeEnum.CS.getType())){
+            elasticSearchService.updateDocumentToType(Constants.CS_INFO_INDEX, "haiguan", id, content);
+        }else if(type.equals(BusiTypeEnum.BS.getType())){
+            elasticSearchService.updateDocumentToType(Constants.BS_INFO_INDEX, "haiguan", id, content);
+        }
 
     }
 
