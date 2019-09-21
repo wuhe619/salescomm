345d0eb5ab9cf6ee314c168d6209f7a90d7a6d73
chengning <chengning@salescomm.net>
2019-09-21 23:17:36 +0800

    feat:报关单主单至海关

--- src/main/java/com/bdaim/customs/services/BgdZService.java
+++ src/main/java/com/bdaim/customs/services/BgdZService.java
@@ -20,10 +20,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /***
  * 报关单.主单
@@ -79,18 +76,109 @@ public class BgdZService implements BusiService{
 	}
 
 	@Override
-	public void updateInfo(String busiType, String cust_id, String cust_group_id, Long cust_user_id, Long id, JSONObject info) {
-		/*HBusiDataManager manager = hBusiDataManagerDao.get(id);
-		if (manager == null) {
-			throw new Exception("修改的数据不存在");
+	public void updateInfo(String busiType, String cust_id, String cust_group_id, Long cust_user_id, Long id, JSONObject info) throws Exception {
+		// 提交至海关平台
+		if ("HAIGUAN".equals(info.getString("_rule_"))) {
+			String sql = "select content, cust_id, cust_group_id, cust_user_id, create_id, create_date ,ext_1, ext_2, ext_3, ext_4, ext_5 from h_data_manager where type=? and id=? ";
+			Map m = jdbcTemplate.queryForMap(sql, busiType, id);
+			if ("1".equals(String.valueOf(m.get("ext_1")))) {
+				log.warn("报关单:[" + id + "]已提交至海关");
+				throw new Exception("报关单:[" + id + "]已提交至海关");
+			}
+			// 更新报关单主单信息
+			String content = (String) m.get("content");
+			JSONObject jo = JSONObject.parseObject(content);
+			jo.put("ext_1", "1");
+			jo.put("send_status", "1");
+			jo.put("id", m.get("id"));
+			jo.put("cust_id", m.get("cust_id"));
+			jo.put("cust_group_id", m.get("cust_group_id"));
+			jo.put("cust_user_id", m.get("cust_user_id"));
+			jo.put("create_id", m.get("create_id"));
+			jo.put("create_date", m.get("create_date"));
+			jo.put("update_id", m.get("update_id"));
+			jo.put("update_date", m.get("update_date"));
+			if (m.get("ext_1") != null && !"".equals(m.get("ext_1")))
+				jo.put("ext_1", m.get("ext_1"));
+			if (m.get("ext_2") != null && !"".equals(m.get("ext_2")))
+				jo.put("ext_2", m.get("ext_2"));
+			if (m.get("ext_3") != null && !"".equals(m.get("ext_3")))
+				jo.put("ext_3", m.get("ext_3"));
+			if (m.get("ext_4") != null && !"".equals(m.get("ext_4")))
+				jo.put("ext_4", m.get("ext_4"));
+			if (m.get("ext_5") != null && !"".equals(m.get("ext_5")))
+				jo.put("ext_5", m.get("ext_5"));
+
+			sql = "UPDATE h_data_manager SET ext_1 = '1', ext_date1 = NOW(), content=? WHERE id = ? AND ext_1 <>'1' ";
+			jdbcTemplate.update(sql, jo.toJSONString(), id);
+			updateDataToES(BusiTypeEnum.SZ.getType(), id.toString(), jo);
+
+			//更新报关单分单信息
+			String selectSql = "select content, cust_id, cust_group_id, cust_user_id, create_id, create_date ,ext_1, ext_2, ext_3, ext_4, ext_5 from h_data_manager JSON_EXTRACT(content, '$.pid')=? AND ext_1 <>'1' ";
+			List<Map<String, Object>> ds = jdbcTemplate.queryForList(selectSql, id);
+			String updateSql = " UPDATE h_data_manager SET ext_1 = '1', ext_date1 = NOW(), content=? WHERE JSON_EXTRACT(content, '$.pid')=? AND ext_1 <>'1' ";
+			for (int i = 0; i < ds.size(); i++) {
+				m = ds.get(i);
+				content = (String) m.get("content");
+				jo = JSONObject.parseObject(content);
+				jo.put("ext_1", "1");
+				jo.put("send_status", "1");
+				jo.put("id", m.get("id"));
+				jo.put("cust_id", m.get("cust_id"));
+				jo.put("cust_group_id", m.get("cust_group_id"));
+				jo.put("cust_user_id", m.get("cust_user_id"));
+				jo.put("create_id", m.get("create_id"));
+				jo.put("create_date", m.get("create_date"));
+				jo.put("update_id", m.get("update_id"));
+				jo.put("update_date", m.get("update_date"));
+				if (m.get("ext_1") != null && !"".equals(m.get("ext_1")))
+					jo.put("ext_1", m.get("ext_1"));
+				if (m.get("ext_2") != null && !"".equals(m.get("ext_2")))
+					jo.put("ext_2", m.get("ext_2"));
+				if (m.get("ext_3") != null && !"".equals(m.get("ext_3")))
+					jo.put("ext_3", m.get("ext_3"));
+				if (m.get("ext_4") != null && !"".equals(m.get("ext_4")))
+					jo.put("ext_4", m.get("ext_4"));
+				if (m.get("ext_5") != null && !"".equals(m.get("ext_5")))
+					jo.put("ext_5", m.get("ext_5"));
+				jdbcTemplate.update(updateSql, jo.toJSONString(), id);
+				updateDataToES(BusiTypeEnum.SF.getType(), String.valueOf(m.get("id")), jo);
+
+			}
+
 		}
-		String content = manager.getContent();
-		MainDan dbjson = JSON.parseObject(content, MainDan.class);
-		BeanUtils.copyProperties(mainDan, dbjson);
-		manager.setContent(JSON.toJSONString(dbjson));
-		hBusiDataManagerDao.save(manager);
-		updateDataToES(manager, Integer.valueOf(id));*/
-		
+
+
+	}
+
+	/**
+	 * 更新es
+	 *
+	 * @param type
+	 * @param id
+	 * @param content
+	 */
+	private void updateDataToES(String type, String id, JSONObject content) {
+		if (type.equals(BusiTypeEnum.SZ.getType())) {
+			elasticSearchService.updateDocumentToType(Constants.SZ_INFO_INDEX, "haiguan", id, content);
+		} else if (type.equals(BusiTypeEnum.CZ.getType())) {
+			elasticSearchService.updateDocumentToType(Constants.CZ_INFO_INDEX, "haiguan", id, content);
+		} else if (type.equals(BusiTypeEnum.BZ.getType())) {
+			elasticSearchService.updateDocumentToType(Constants.BZ_INFO_INDEX, "haiguan", id, content);
+		} else if (type.equals(BusiTypeEnum.SF.getType())) {
+			elasticSearchService.updateDocumentToType(Constants.SF_INFO_INDEX, "haiguan", id, content);
+		} else if (type.equals(BusiTypeEnum.CF.getType())) {
+			elasticSearchService.updateDocumentToType(Constants.CF_INFO_INDEX, "haiguan", id, content);
+		} else if (type.equals(BusiTypeEnum.BF.getType())) {
+			elasticSearchService.updateDocumentToType(Constants.BF_INFO_INDEX, "haiguan", id, content);
+		} else if (type.equals(BusiTypeEnum.SS.getType())) {
+			elasticSearchService.updateDocumentToType(Constants.SS_INFO_INDEX, "haiguan", id, content);
+		} else if (type.equals(BusiTypeEnum.CS.getType())) {
+			elasticSearchService.updateDocumentToType(Constants.CS_INFO_INDEX, "haiguan", id, content);
+		} else if (type.equals(BusiTypeEnum.BS.getType())) {
+			elasticSearchService.updateDocumentToType(Constants.BS_INFO_INDEX, "haiguan", id, content);
+		}
+
 	}
 
 	@Override
