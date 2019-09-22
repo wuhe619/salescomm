47385fe198b06abe2b60546ba92b8252f912b61c
wuhe <wuhe@bdaim.com>
2019-09-22 16:54:53 +0800

    申报单分单添加商品

---
+++ src/main/java/com/bdaim/customs/services/BgdHzService.java
@@ -0,0 +1,40 @@
+package com.bdaim.customs.services;
+
+import com.alibaba.fastjson.JSONObject;
+import com.bdaim.common.service.BusiService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("busi_bgd_hz")
+public class BgdHzService implements BusiService {
+    @Override
+    public void insertInfo(String busiType, String cust_id, String cust_group_id, Long cust_user_id, Long id, JSONObject info) throws Exception {
+
+    }
+
+    @Override
+    public void updateInfo(String busiType, String cust_id, String cust_group_id, Long cust_user_id, Long id, JSONObject info) throws Exception {
+
+    }
+
+    @Override
+    public void getInfo(String busiType, String cust_id, String cust_group_id, Long cust_user_id, Long id, JSONObject info, JSONObject param) {
+
+    }
+
+    @Override
+    public void deleteInfo(String busiType, String cust_id, String cust_group_id, Long cust_user_id, Long id) throws Exception {
+
+    }
+
+    @Override
+    public String formatQuery(String busiType, String cust_id, String cust_group_id, Long cust_user_id, JSONObject params, List sqlParams) {
+        return null;
+    }
+
+    @Override
+    public void formatInfo(String busiType, String cust_id, String cust_group_id, Long cust_user_id, JSONObject info) {
+
+    }
+}
