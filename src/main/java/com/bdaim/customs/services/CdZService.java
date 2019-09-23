57f41748a3b392dd232cb451715240261682b999
wuhe <wuhe@bdaim.com>
2019-09-23 17:16:24 +0800

    场站信息同步接口

--- src/main/java/com/bdaim/customs/services/CdZService.java
+++ src/main/java/com/bdaim/customs/services/CdZService.java
@@ -71,7 +71,7 @@ public class CdZService implements BusiService {
             for (int i = 0; i < dataList.size(); i++) {
                 HBusiDataManager dm = dataList.get(i);
                 serviceUtils.addDataToES(dm.getId().toString(),dm.getType(),JSON.parseObject(dm.getContent()));
-                if (!dm.getType().equals(BusiTypeEnum.CZ.getType())) {
+                if (dm.getType().equals(BusiTypeEnum.CZ.getType())) {
                     index = i;
                 }
             }
@@ -231,7 +231,7 @@ public class CdZService implements BusiService {
 //		String content = json.toJSONString();
         CZ.setContent(info.toJSONString());
         dataList.add(CZ);
-        List<HBusiDataManager> parties = serviceUtils.getDataList(BusiTypeEnum.CF.getType(),info.getLong("fromSbzId"));
+        List<HBusiDataManager> parties = serviceUtils.getDataList(BusiTypeEnum.SF.getType(),info.getLong("fromSbzId"));
         for (HBusiDataManager hp : parties) {
             HBusiDataManager hm = new HBusiDataManager();
             hm.setType(BusiTypeEnum.CF.getType());
@@ -247,7 +247,7 @@ public class CdZService implements BusiService {
             _content.put("main_bill_no", json.get("bill_no"));
             hm.setContent(_content.toJSONString());
             dataList.add(hm);
-            List<HBusiDataManager> goods = serviceUtils.getDataList(BusiTypeEnum.CS.getType(),hp.getId().longValue());
+            List<HBusiDataManager> goods = serviceUtils.getDataList(BusiTypeEnum.BS.getType(),hp.getId().longValue());
             for (HBusiDataManager gp : goods) {
                 HBusiDataManager good = new HBusiDataManager();
                 gp.setType(BusiTypeEnum.CS.getType());
