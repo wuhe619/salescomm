a851a308a21a17a9b360fb63a53730ec5d1f2483
chengning <chengning@bdaim.com>
2019-09-29 18:53:46 +0800

    feat:#

--- src/main/java/com/bdaim/common/service/SequenceService.java
+++ src/main/java/com/bdaim/common/service/SequenceService.java
@@ -6,37 +6,39 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Map;
 
 @Service
+@Transactional
 public class SequenceService {
     static private Logger logger = LoggerFactory.getLogger(SequenceService.class);
 
     @Resource
     private JdbcTemplate jdbcTemplate;
 
-    public Long getSeq(String type) throws Exception{
+    public synchronized Long getSeq(String type) throws Exception {
         Long seq = 1L;
         try {
-            synchronized (type) {
-                String sql = "select value from sys_sequences where name='"+type+"'";
-                List<Map<String, Object>> r = jdbcTemplate.queryForList(sql);
-                if(r.size()==0) {
-                    seq=1L;
-                    sql = "insert into sys_sequences(name,value) value('"+type+"', "+seq+")";
-                    jdbcTemplate.update(sql);
-                }else {
-                    seq = (Long)r.get(0).get("value");
-                    if(seq<=0)
-                        seq=1L;
-                    seq++;
-                    sql = "update sys_sequences set value="+seq+" where name='"+type+"'";
-                    jdbcTemplate.update(sql);
-                }
+            //synchronized (type) {
+            String sql = "select value from sys_sequences where name='" + type + "'";
+            List<Map<String, Object>> r = jdbcTemplate.queryForList(sql);
+            if (r.size() == 0) {
+                seq = 1L;
+                sql = "insert into sys_sequences(name,value) value('" + type + "', " + seq + ")";
+                jdbcTemplate.update(sql);
+            } else {
+                seq = (Long) r.get(0).get("value");
+                if (seq <= 0)
+                    seq = 1L;
+                seq++;
+                sql = "update sys_sequences set value=" + seq + " where name='" + type + "'";
+                jdbcTemplate.update(sql);
             }
-        }catch(Exception e) {
-            logger.error("获取主键ID异常:["+type+"] ",e);
+            //}
+        } catch (Exception e) {
+            logger.error("获取主键ID异常:[" + type + "] ", e);
             throw new Exception("获取主键ID异常");
         }
 
