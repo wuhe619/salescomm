9c001e495261cc05bba2677b135f994daf46a2d3
chengning <chengning@bdaim.com>
2019-10-11 12:16:39 +0800

    feat:删除分单身份图片数量更新

--- src/main/java/com/bdaim/customs/services/ExportExcelService.java
+++ src/main/java/com/bdaim/customs/services/ExportExcelService.java
@@ -22,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
@@ -75,10 +76,10 @@ public class ExportExcelService {
         }
 
         Workbook workbook = ExcelExportUtil.exportExcel(params, map);
-        /*FileOutputStream fos = new FileOutputStream("/data/chengning/test.xlsx");
+        FileOutputStream fos = new FileOutputStream("/tmp");
         workbook.write(fos);
-        fos.close();*/
-        workbook.write(response.getOutputStream());
+        fos.close();
+        //workbook.write(response.getOutputStream());
         LOG.info("导出:{}完成", templatePath);
     }
 
