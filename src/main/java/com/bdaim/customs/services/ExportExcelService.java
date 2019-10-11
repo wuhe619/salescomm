1e07ed32322919c51fe076cd92eaccc73962bcfd
chengning <chengning@bdaim.com>
2019-10-11 11:56:27 +0800

    feat:导出

--- src/main/java/com/bdaim/customs/services/ExportExcelService.java
+++ src/main/java/com/bdaim/customs/services/ExportExcelService.java
@@ -22,7 +22,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
@@ -76,10 +75,10 @@ public class ExportExcelService {
         }
 
         Workbook workbook = ExcelExportUtil.exportExcel(params, map);
-        FileOutputStream fos = new FileOutputStream("/data/chengning/test.xlsx");
+        /*FileOutputStream fos = new FileOutputStream("/data/chengning/test.xlsx");
         workbook.write(fos);
-        fos.close();
-        //workbook.write(response.getOutputStream());
+        fos.close();*/
+        workbook.write(response.getOutputStream());
         LOG.info("导出:{}完成", templatePath);
     }
 
