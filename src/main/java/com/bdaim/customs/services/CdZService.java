1c6a677ea6ce2a0b3aa8cd2be7e5bf3f9e7e84a0
wuhe <wuhe@bdaim.com>
2019-10-11 22:12:22 +0800

    按类型分表

--- src/main/java/com/bdaim/customs/services/CdZService.java
+++ src/main/java/com/bdaim/customs/services/CdZService.java
@@ -220,7 +220,6 @@ public class CdZService implements BusiService {
             serviceUtils.updateDataToES(BusiTypeEnum.CZ.getType(), id.toString(), jo);
 
             //更新舱单分单信息
-            //String selectSql = "select id, type, content, cust_id, cust_group_id, cust_user_id, create_id, create_date ,ext_1, ext_2, ext_3, ext_4, ext_5 from " + HMetaDataDef.getTable(BusiTypeEnum.CF.getType(), "") + " WHERE ( CASE WHEN JSON_VALID(content) THEN JSON_EXTRACT(content, '$.pid')=? ELSE null END  or CASE WHEN JSON_VALID(content) THEN JSON_EXTRACT(content, '$.pid')=? ELSE null END) AND type = ? AND IFNULL(ext_1,'') <>'1' ";
             String selectSql = "select id, type, content, cust_id, cust_group_id, cust_user_id, create_id, create_date ,ext_1, ext_2, ext_3, ext_4, ext_5 from " + HMetaDataDef.getTable(BusiTypeEnum.CF.getType(), "") + " WHERE ext_4=(SELECT ext_3 FROM " + HMetaDataDef.getTable(BusiTypeEnum.getParentType(busiType), "") + " WHERE id = ?) AND type = ? AND IFNULL(ext_1,'') <>'1' ";
             List<Map<String, Object>> ds = jdbcTemplate.queryForList(selectSql, id, BusiTypeEnum.CF.getType());
             String updateSql = " UPDATE " + HMetaDataDef.getTable(BusiTypeEnum.CF.getType(), "") + " SET ext_1 = '1', ext_date1 = NOW(), content=? WHERE id =? AND type = ? AND IFNULL(ext_1,'') <>'1' ";
@@ -251,10 +250,11 @@ public class CdZService implements BusiService {
                 jdbcTemplate.update(updateSql, jo.toJSONString(), m.get("id"), BusiTypeEnum.CF.getType());
                 serviceUtils.updateDataToES(BusiTypeEnum.CF.getType(), String.valueOf(m.get("id")), jo);
 
-                //start to create xml file
-                String xmlString = cangdanXmlEXP311.createXml(m, ds);
-                info.put("xml",xmlString);
             }
+
+            //start to create xml file
+            String xmlString = cangdanXmlEXP311.createXml(m, ds);
+            info.put("xml",xmlString);
         }
 
     }
