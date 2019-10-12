7f876a288e7e0a67d32c79973d39b4f0c3d49385
chengning <chengning@bdaim.com>
2019-10-12 19:27:11 +0800

    feat:申报单主单排序

--- src/main/java/com/bdaim/customs/services/SbdZService.java
+++ src/main/java/com/bdaim/customs/services/SbdZService.java
@@ -328,6 +328,8 @@ public class SbdZService implements BusiService {
     @Override
     public String formatQuery(String busiType, String cust_id, String cust_group_id, Long cust_user_id, JSONObject params, List sqlParams) {
         StringBuffer sqlstr = new StringBuffer("select id, content , cust_id, create_id, create_date,ext_1, ext_2, ext_3, ext_4, ext_5 from " + HMetaDataDef.getTable(busiType, "") + " where type=?");
+        String _orderby_ = params.getString("_orderby_");
+        String _sort_ = params.getString("_sort_");
         if (!"all".equals(cust_id))
             sqlstr.append(" and cust_id='").append(cust_id).append("'");
         sqlParams.add(busiType);
@@ -343,7 +345,8 @@ public class SbdZService implements BusiService {
         while (keys.hasNext()) {
             String key = (String) keys.next();
             if (StringUtil.isEmpty(String.valueOf(params.get(key)))) continue;
-            if ("pageNum".equals(key) || "pageSize".equals(key) || "stationId".equals(key) || "cust_id".equals(key))
+            if ("pageNum".equals(key) || "pageSize".equals(key) || "stationId".equals(key) || "cust_id".equals(key)
+                    || "cust_id".equals(key) || "_sort_".equals(key) || "_orderby_".equals(key))
                 continue;
             if ("cust_id".equals(key)) {
                 sqlstr.append(" and cust_id=?");
@@ -391,7 +394,10 @@ public class SbdZService implements BusiService {
 
             sqlParams.add(params.get(key));
         }
-        sqlstr.append(" ORDER BY create_date DESC, update_date DESC ");
+        //sqlstr.append(" ORDER BY create_date DESC, update_date DESC ");
+        if (StringUtil.isNotEmpty(_orderby_) && StringUtil.isNotEmpty(_sort_)) {
+            sqlstr.append(" ORDER BY ").append(_orderby_).append(" ").append(_sort_);
+        }
         return sqlstr.toString();
     }
 
