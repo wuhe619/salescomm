ce0fcb3cbfef4aa7473292636cc2e10d952b8a84
li yang <liyang@hunktimes.com>
2019-10-30 10:53:54 +0800

    销售定价:查询及修改

---
+++ src/main/java/com/bdaim/customer/service/CustomerPriceSettingService.java
@@ -0,0 +1,55 @@
+package com.bdaim.customer.service;
+
+import com.alibaba.fastjson.JSON;
+import com.bdaim.customer.dao.CustomerPropertyRepository;
+import com.bdaim.customer.entity.CustomerProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class CustomerPriceSettingService {
+    @Autowired
+    private CustomerPropertyRepository customerPropertyRepository;
+
+    public String queryPrice(String custId) {
+        CustomerProperty property =
+                customerPropertyRepository.findByCustIdAndPropertyName(custId, "10000_config");
+        if (property == null) {
+            savePrice(custId);
+        }
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", 200);
+        map.put("propertyValue", property.getPropertyValue());
+        return JSON.toJSONString(map);
+    }
+
+    public String updatePrice(String custId, String price) {
+        CustomerProperty property =
+                customerPropertyRepository.findByCustIdAndPropertyName(custId, "10000_config");
+        if (property == null) {
+            property = savePrice(custId);
+        }
+        property.setPropertyValue(price);
+        Map<String, Object> map = new HashMap<>();
+        customerPropertyRepository.save(property);
+        map.put("code", 200);
+        map.put("propertyValue", property.getPropertyValue());
+         return JSON.toJSONString(map);
+    }
+
+    public CustomerProperty savePrice(String custId) {
+        CustomerProperty property = new CustomerProperty();
+        property.setCustId(custId);
+        property.setPropertyName("10000_config");
+        property.setPropertyValue("0");
+        property.setCreateTime(new Timestamp(new Date().getTime()));
+        customerPropertyRepository.save(property);
+
+        return property;
+    }
+}
