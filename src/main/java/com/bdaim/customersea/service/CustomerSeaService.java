7039a23ccba738da91714994233f246ba760c333
li yang <liyang@hunktimes.com>
2019-11-27 10:58:38 +0800

    #

--- src/main/java/com/bdaim/customersea/service/CustomerSeaService.java
+++ src/main/java/com/bdaim/customersea/service/CustomerSeaService.java
@@ -4322,7 +4322,7 @@ public class CustomerSeaService {
             CustomerUser user;
             List<CustomerSeaProperty> properties;
             List<Map<String, Object>> stat;
-            String statSql = "SELECT COUNT(status=0 OR null) sumCount,IFNULL(COUNT(super_data like ''%\"SYS007\":\"未跟进\"%'' AND status = 0 OR null),0) AS noFollowSum, IFNULL(COUNT(`status` = 1 OR null),0) AS clueSurplusSum, IFNULL(COUNT(`call_fail_count` >= 1 OR null),0) AS failCallSum,(count(distinct super_data->'$.SYS014')) as custType  FROM " + ConstantsUtil.SEA_TABLE_PREFIX + "{0} WHERE 1=1  ";
+            String statSql = "SELECT COUNT(status=0 OR null) sumCount,IFNULL(COUNT(super_data->>'$.SYS007' like '%未跟进%' AND status = 0 OR null),0) AS noFollowSum, IFNULL(COUNT(`status` = 1 OR null),0) AS clueSurplusSum, IFNULL(COUNT(`call_fail_count` >= 1 OR null),0) AS failCallSum,(count(distinct super_data->'$.SYS014')) as custType  FROM " + ConstantsUtil.SEA_TABLE_PREFIX + "{0} WHERE 1=1  ";
             MarketProjectProperty executionGroup;
             StringBuilder userGroupName;
             CustomerUserGroup customerUserGroup;
@@ -4404,6 +4404,7 @@ public class CustomerSeaService {
                         }
                     }
                     appSql.append(" group by super_data->>'$.SYS014' ");
+                    System.err.println(statSql);
                     stat = customerSeaDao.sqlQuery(MessageFormat.format(statSql, String.valueOf(customerSea.getId())));
                     dto.setClueSurplusSum(NumberConvertUtil.parseLong(stat.get(0).get("clueSurplusSum")));
                     dto.setFailCallSum(NumberConvertUtil.parseLong(stat.get(0).get("failCallSum")));
