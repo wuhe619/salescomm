b7a8c7cb72e92fa4ac200492bcf2bcb394560a01
li yang <liyang@hunktimes.com>
2019-12-16 14:22:33 +0800

    #

--- src/main/java/com/bdaim/customs/services/SbdZService.java
+++ src/main/java/com/bdaim/customs/services/SbdZService.java
@@ -418,79 +418,79 @@ public class SbdZService implements BusiService {
     @Override
     public String formatQuery(String busiType, String cust_id, String cust_group_id, Long cust_user_id, JSONObject params, List sqlParams) {
 //        if (StringUtil.isEmpty(params.getString("_rule_")) && !"SBDCHECK".equals(params.getString("_rule_"))) {
-            StringBuffer sqlstr = new StringBuffer("select id, content , cust_id, create_id, create_date,ext_1, ext_2, ext_3, ext_4, ext_5 from " + HMetaDataDef.getTable(busiType, "") + " where type=?");
-            String _orderby_ = params.getString("_orderby_");
-            String _sort_ = params.getString("_sort_");
-            if (!"all".equals(cust_id))
-                sqlstr.append(" and cust_id='").append(cust_id).append("'");
-            sqlParams.add(busiType);
-            String stationId = params.getString("stationId");
-            // 处理场站检索
-            if (StringUtil.isNotEmpty(stationId)) {
-                String stationSql = "SELECT cust_id FROM t_customer_property WHERE property_name='station_id' AND property_value = ?";
-                sqlstr.append(" and cust_id IN ( ").append(stationSql).append(" )");
-                sqlParams.add(stationId);
-            }
+        StringBuffer sqlstr = new StringBuffer("select id, content , cust_id, create_id, create_date,ext_1, ext_2, ext_3, ext_4, ext_5 from " + HMetaDataDef.getTable(busiType, "") + " where type=?");
+        String _orderby_ = params.getString("_orderby_");
+        String _sort_ = params.getString("_sort_");
+        if (!"all".equals(cust_id))
+            sqlstr.append(" and cust_id='").append(cust_id).append("'");
+        sqlParams.add(busiType);
+        String stationId = params.getString("stationId");
+        // 处理场站检索
+        if (StringUtil.isNotEmpty(stationId)) {
+            String stationSql = "SELECT cust_id FROM t_customer_property WHERE property_name='station_id' AND property_value = ?";
+            sqlstr.append(" and cust_id IN ( ").append(stationSql).append(" )");
+            sqlParams.add(stationId);
+        }
 
-            Iterator keys = params.keySet().iterator();
-            while (keys.hasNext()) {
-                String key = (String) keys.next();
-                if (StringUtil.isEmpty(String.valueOf(params.get(key)))) continue;
-                if ("pageNum".equals(key) || "pageSize".equals(key) || "stationId".equals(key) || "cust_id".equals(key)
-                        || "cust_id".equals(key) || "_sort_".equals(key) || "_orderby_".equals(key))
-                    continue;
-                if ("cust_id".equals(key)) {
-                    sqlstr.append(" and cust_id=?");
-                } else if (key.equals("bill_no")) {
-                    sqlstr.append(" and ext_3 = ? ");
-                } else if (key.startsWith("_c_")) {
-                    sqlstr.append(" and JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(3) + "') like concat('%',?,'%')");
-                } else if (key.startsWith("_g_")) {
-                    sqlstr.append(" and JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(3) + "') > ?");
-                } else if (key.startsWith("_ge_")) {
-                    sqlstr.append(" and JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(4) + "') >= ?");
-                } else if (key.startsWith("_l_")) {
-                    sqlstr.append(" and JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(3) + "') < ?");
-                } else if (key.startsWith("_le_")) {
-                    sqlstr.append(" and JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(4) + "') <= ?");
-                } else if (key.startsWith("_range_")) {
-                    if ("0".equals(String.valueOf(params.get(key)))) {
-                        sqlstr.append(" and ( JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(7) + "') <= ?")
-                                .append(" OR JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(7) + "') = '' ")
-                                .append(" OR JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(7) + "') IS NULL ) ");
-                    } else {
-                        sqlstr.append(" and JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(7) + "') >= ?");
-                    }
-                } else if ("commit_status".equals(key)) {
-                    // 提交记录特殊处理
-                    if ("1".equals(String.valueOf(params.get(key)))) {
-                        //  未提交
-                        sqlstr.append(" AND ( JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$.commit_cangdan_status') = 'N' OR JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$.commit_cangdan_status') = '' )  ")
-                                .append(" AND ( JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$.commit_baodan_status') = 'N' OR JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$.commit_baodan_status') = '' )  ");
-                    } else if ("2".equals(String.valueOf(params.get(key)))) {
-                        //  舱单已提交
-                        sqlstr.append(" AND JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$.commit_cangdan_status') = 'Y' ");
-                    } else if ("3".equals(String.valueOf(params.get(key)))) {
-                        //  报单已提交
-                        sqlstr.append(" AND JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$.commit_baodan_status') = 'Y' ");
-                    } else if ("4".equals(String.valueOf(params.get(key)))) {
-                        //  舱单 报单都提交
-                        sqlstr.append(" AND ( JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$.commit_cangdan_status') = 'Y' AND JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$.commit_baodan_status') = 'Y' ) ");
-                    }
-                    continue;
-
+        Iterator keys = params.keySet().iterator();
+        while (keys.hasNext()) {
+            String key = (String) keys.next();
+            if (StringUtil.isEmpty(String.valueOf(params.get(key)))) continue;
+            if ("pageNum".equals(key) || "pageSize".equals(key) || "stationId".equals(key) || "cust_id".equals(key)
+                    || "cust_id".equals(key) || "_sort_".equals(key) || "_orderby_".equals(key))
+                continue;
+            if ("cust_id".equals(key)) {
+                sqlstr.append(" and cust_id=?");
+            } else if (key.equals("bill_no")) {
+                sqlstr.append(" and ext_3 = ? ");
+            } else if (key.startsWith("_c_")) {
+                sqlstr.append(" and JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(3) + "') like concat('%',?,'%')");
+            } else if (key.startsWith("_g_")) {
+                sqlstr.append(" and JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(3) + "') > ?");
+            } else if (key.startsWith("_ge_")) {
+                sqlstr.append(" and JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(4) + "') >= ?");
+            } else if (key.startsWith("_l_")) {
+                sqlstr.append(" and JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(3) + "') < ?");
+            } else if (key.startsWith("_le_")) {
+                sqlstr.append(" and JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(4) + "') <= ?");
+            } else if (key.startsWith("_range_")) {
+                if ("0".equals(String.valueOf(params.get(key)))) {
+                    sqlstr.append(" and ( JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(7) + "') <= ?")
+                            .append(" OR JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(7) + "') = '' ")
+                            .append(" OR JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(7) + "') IS NULL ) ");
                 } else {
-                    sqlstr.append(" and JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key + "')=?");
+                    sqlstr.append(" and JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key.substring(7) + "') >= ?");
                 }
+            } else if ("commit_status".equals(key)) {
+                // 提交记录特殊处理
+                if ("1".equals(String.valueOf(params.get(key)))) {
+                    //  未提交
+                    sqlstr.append(" AND ( JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$.commit_cangdan_status') = 'N' OR JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$.commit_cangdan_status') = '' )  ")
+                            .append(" AND ( JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$.commit_baodan_status') = 'N' OR JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$.commit_baodan_status') = '' )  ");
+                } else if ("2".equals(String.valueOf(params.get(key)))) {
+                    //  舱单已提交
+                    sqlstr.append(" AND JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$.commit_cangdan_status') = 'Y' ");
+                } else if ("3".equals(String.valueOf(params.get(key)))) {
+                    //  报单已提交
+                    sqlstr.append(" AND JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$.commit_baodan_status') = 'Y' ");
+                } else if ("4".equals(String.valueOf(params.get(key)))) {
+                    //  舱单 报单都提交
+                    sqlstr.append(" AND ( JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$.commit_cangdan_status') = 'Y' AND JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$.commit_baodan_status') = 'Y' ) ");
+                }
+                continue;
 
-                sqlParams.add(params.get(key));
-            }
-            //sqlstr.append(" ORDER BY create_date DESC, update_date DESC ");
-            if (StringUtil.isNotEmpty(_orderby_) && StringUtil.isNotEmpty(_sort_)) {
-                sqlstr.append(" ORDER BY ").append(_orderby_).append(" ").append(_sort_);
+            } else {
+                sqlstr.append(" and JSON_EXTRACT(REPLACE(REPLACE(REPLACE(content,'\t', ''),CHAR(13),'') ,CHAR(10),''), '$." + key + "')=?");
             }
 
-            return sqlstr.toString();
+            sqlParams.add(params.get(key));
+        }
+        //sqlstr.append(" ORDER BY create_date DESC, update_date DESC ");
+        if (StringUtil.isNotEmpty(_orderby_) && StringUtil.isNotEmpty(_sort_)) {
+            sqlstr.append(" ORDER BY ").append(_orderby_).append(" ").append(_sort_);
+        }
+
+        return sqlstr.toString();
 
     }
 
@@ -860,6 +860,8 @@ public class SbdZService implements BusiService {
                 return dataMap;
             }
             if (d > weight) {
+                log.info("毛重：" + weight);
+                log.info("重量：" + d);
                 dataMap.put("code", 2000);
                 dataMap.put("message", "分单:[" + ext_3 + "],商品重量之和大于分单的毛重");
                 return dataMap;
