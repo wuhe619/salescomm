156e94740af4e7ed95cac157b5a8b07bfb707c3b
li yang <liyang@hunktimes.com>
2019-12-16 18:15:06 +0800

    #

--- src/main/java/com/bdaim/customs/services/BgdZService.java
+++ src/main/java/com/bdaim/customs/services/BgdZService.java
@@ -265,11 +265,11 @@ public class BgdZService implements BusiService {
                             param.remove("send_status");
                         }
                         List products;
-                        if ("_export_bgd_z_main_data".equals(param.getString("_rule_"))||"_export_bgd_z_main_data_inspection".equals(param.getString("_rule_"))) {
+//                        if ("_export_bgd_z_main_data".equals(param.getString("_rule_"))||"_export_bgd_z_main_data_inspection".equals(param.getString("_rule_"))) {
                             products = serviceUtils.listSdByBillNos1(cust_id, BusiTypeEnum.BS.getType(), BusiTypeEnum.BF.getType(), main_bill_no, partyBillNos, param);
-                        } else {
-                            products = serviceUtils.listSdByBillNos(cust_id, BusiTypeEnum.BS.getType(), main_bill_no, partyBillNos, param);
-                        }
+//                        } else {
+//                            products = serviceUtils.listSdByBillNos(cust_id, BusiTypeEnum.BS.getType(), main_bill_no, partyBillNos, param);
+//                        }
 
                         log.info("报关单：{}分单数量:{}", main_bill_no, singles);
                         log.info("报关单：{}商品数量:{}", main_bill_no, products);
