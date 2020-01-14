f516b0ea714f86447fe54b6b0feb41926bb0596b
chengning <chengning@bdaim.com>
2020-01-14 10:41:29 +0800

    feat:#

---
+++ src/main/java/com/bdaim/crm/EntInfo.java
@@ -0,0 +1,249 @@
+package com.bdaim.crm;
+
+import java.util.List;
+
+/**
+ * 企业基本信息
+ */
+public class EntInfo {
+    private String entName;
+    private String legalName;
+    private String regCap;
+    private String estiblishTime;
+    private String cancelDate;
+    private String entStatus;
+    private String regNo;
+    private String orgNo;
+    private String creditCode;
+    private String taxNo;
+    private String entTypeCode;
+    private String entType;
+    private String industryCode;
+    private String industry;
+    private  String fromTime;
+    private String toTime;
+    private String approvedTime;
+    private String regInstitute;
+    private String regProvinceCode;
+    private String regProvinceName;
+    private String regLocation;
+    private String regCityCode;
+    private String regCityName;
+    private String opScope;
+    private List<String> phoneNumbers;
+    private List<String> email;
+
+    public String getEntName() {
+        return entName;
+    }
+
+    public void setEntName(String entName) {
+        this.entName = entName;
+    }
+
+    public String getLegalName() {
+        return legalName;
+    }
+
+    public void setLegalName(String legalName) {
+        this.legalName = legalName;
+    }
+
+    public String getRegCap() {
+        return regCap;
+    }
+
+    public void setRegCap(String regCap) {
+        this.regCap = regCap;
+    }
+
+    public String getEstiblishTime() {
+        return estiblishTime;
+    }
+
+    public void setEstiblishTime(String estiblishTime) {
+        this.estiblishTime = estiblishTime;
+    }
+
+    public String getCancelDate() {
+        return cancelDate;
+    }
+
+    public void setCancelDate(String cancelDate) {
+        this.cancelDate = cancelDate;
+    }
+
+    public String getEntStatus() {
+        return entStatus;
+    }
+
+    public void setEntStatus(String entStatus) {
+        this.entStatus = entStatus;
+    }
+
+    public String getRegNo() {
+        return regNo;
+    }
+
+    public void setRegNo(String regNo) {
+        this.regNo = regNo;
+    }
+
+    public String getOrgNo() {
+        return orgNo;
+    }
+
+    public void setOrgNo(String orgNo) {
+        this.orgNo = orgNo;
+    }
+
+    public String getCreditCode() {
+        return creditCode;
+    }
+
+    public void setCreditCode(String creditCode) {
+        this.creditCode = creditCode;
+    }
+
+    public String getTaxNo() {
+        return taxNo;
+    }
+
+    public void setTaxNo(String taxNo) {
+        this.taxNo = taxNo;
+    }
+
+    public String getEntTypeCode() {
+        return entTypeCode;
+    }
+
+    public void setEntTypeCode(String entTypeCode) {
+        this.entTypeCode = entTypeCode;
+    }
+
+    public String getEntType() {
+        return entType;
+    }
+
+    public void setEntType(String entType) {
+        this.entType = entType;
+    }
+
+    public String getIndustryCode() {
+        return industryCode;
+    }
+
+    public void setIndustryCode(String industryCode) {
+        this.industryCode = industryCode;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getFromTime() {
+        return fromTime;
+    }
+
+    public void setFromTime(String fromTime) {
+        this.fromTime = fromTime;
+    }
+
+    public String getToTime() {
+        return toTime;
+    }
+
+    public void setToTime(String toTime) {
+        this.toTime = toTime;
+    }
+
+    public String getApprovedTime() {
+        return approvedTime;
+    }
+
+    public void setApprovedTime(String approvedTime) {
+        this.approvedTime = approvedTime;
+    }
+
+    public String getRegInstitute() {
+        return regInstitute;
+    }
+
+    public void setRegInstitute(String regInstitute) {
+        this.regInstitute = regInstitute;
+    }
+
+    public String getRegProvinceCode() {
+        return regProvinceCode;
+    }
+
+    public void setRegProvinceCode(String regProvinceCode) {
+        this.regProvinceCode = regProvinceCode;
+    }
+
+    public String getRegProvinceName() {
+        return regProvinceName;
+    }
+
+    public void setRegProvinceName(String regProvinceName) {
+        this.regProvinceName = regProvinceName;
+    }
+
+    public String getRegLocation() {
+        return regLocation;
+    }
+
+    public void setRegLocation(String regLocation) {
+        this.regLocation = regLocation;
+    }
+
+    public String getRegCityCode() {
+        return regCityCode;
+    }
+
+    public void setRegCityCode(String regCityCode) {
+        this.regCityCode = regCityCode;
+    }
+
+    public String getRegCityName() {
+        return regCityName;
+    }
+
+    public void setRegCityName(String regCityName) {
+        this.regCityName = regCityName;
+    }
+
+    public String getOpScope() {
+        return opScope;
+    }
+
+    public void setOpScope(String opScope) {
+        this.opScope = opScope;
+    }
+
+    public List<String> getPhoneNumbers() {
+        return phoneNumbers;
+    }
+
+    public void setPhoneNumbers(List<String> phoneNumbers) {
+        this.phoneNumbers = phoneNumbers;
+    }
+
+    public List<String> getEmail() {
+        return email;
+    }
+
+    public void setEmail(List<String> email) {
+        this.email = email;
+    }
+
+    public static void main(String[] args) {
+        EntInfo entInfo = new EntInfo();
+
+
+    }
+}
