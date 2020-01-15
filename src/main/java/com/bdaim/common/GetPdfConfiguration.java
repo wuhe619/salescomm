2aa102d305b787a0e9f6b7ad6387789b8d48ea1a
chengning <chengning@bdaim.com>
2020-01-15 19:12:16 +0800

    feat:#

--- src/main/java/com/bdaim/common/GetPdfConfiguration.java
+++ src/main/java/com/bdaim/common/GetPdfConfiguration.java
@@ -36,6 +36,6 @@ public class GetPdfConfiguration extends WebSecurityConfigurerAdapter {
                 "/dic/getLoanCost", "/dic/productApply", "/dic/listShowAdSpace", "/dic/getBrandList",
                 "/customerSea/xzCustomerSeaGetPhone", "/upload/pic/**", "/marketResource/getVoice0/**",
                 "/custuser/bindUserOpenId", "/open/customs/terminal/check/**", "/open/phone/xzGetTaskPhone", "/open/phone/xzGetTaskPhone0",
-                "/open/unicom/callBack", "/open/unicom/recordCallBack", "/version", "/admin/achievement/setAchievement");
+                "/open/unicom/callBack", "/open/unicom/recordCallBack", "/achievement/setAchievement", "/achievement/queryAchievementList");
     }
 }
