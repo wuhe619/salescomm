b4d6b8c243e54d013395b65a322bb551c6842b72
chengning <chengning@bdaim.com>
2020-01-16 17:00:33 +0800

    feat:网站 专利 商标实体类

---
+++ src/main/java/com/bdaim/crm/EntWeb.java
@@ -0,0 +1,71 @@
+package com.bdaim.crm;
+
+import java.util.List;
+
+/**
+ * 网站备案信息
+ */
+public class EntWeb {
+
+    /**
+     * 网站首页(多个)
+     */
+    private List<String> homePages;
+
+    /**
+     * 网站名称
+     */
+    private String webName;
+
+    /**
+     * 域名(多个)
+     */
+    private List<String> domains;
+
+    /**
+     * 备案号
+     */
+    private String icp;
+
+    public List<String> getHomePages() {
+        return homePages;
+    }
+
+    public void setHomePages(List<String> homePages) {
+        this.homePages = homePages;
+    }
+
+    public String getWebName() {
+        return webName;
+    }
+
+    public void setWebName(String webName) {
+        this.webName = webName;
+    }
+
+    public List<String> getDomains() {
+        return domains;
+    }
+
+    public void setDomains(List<String> domains) {
+        this.domains = domains;
+    }
+
+    public String getIcp() {
+        return icp;
+    }
+
+    public void setIcp(String icp) {
+        this.icp = icp;
+    }
+
+    @Override
+    public String toString() {
+        return "EntWeb{" +
+                "homePages=" + homePages +
+                ", webName='" + webName + '\'' +
+                ", domains=" + domains +
+                ", icp='" + icp + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/bdaim/crm/TrademarkInfo.java
@@ -0,0 +1,126 @@
+package com.bdaim.crm;
+
+/**
+ * 商标信息
+ */
+public class TrademarkInfo {
+
+    /**
+     * 商标图片地址
+     */
+    private String trademarkPic;
+
+    /**
+     * 商标名称
+     */
+    private String name;
+
+    /**
+     * 注册号
+     */
+    private String regNo;
+
+    /**
+     * 国际分类
+     */
+    private String classify;
+
+    /**
+     * 申请时间
+     */
+
+    private Long applyTime;
+
+    /**
+     * 申请人
+     */
+    private String applicant;
+
+    /**
+     * 商标类型
+     */
+    private String type;
+
+    /**
+     * 商标状态
+     */
+    private String status;
+
+    public String getTrademarkPic() {
+        return trademarkPic;
+    }
+
+    public void setTrademarkPic(String trademarkPic) {
+        this.trademarkPic = trademarkPic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegNo() {
+        return regNo;
+    }
+
+    public void setRegNo(String regNo) {
+        this.regNo = regNo;
+    }
+
+    public String getClassify() {
+        return classify;
+    }
+
+    public void setClassify(String classify) {
+        this.classify = classify;
+    }
+
+    public Long getApplyTime() {
+        return applyTime;
+    }
+
+    public void setApplyTime(Long applyTime) {
+        this.applyTime = applyTime;
+    }
+
+    public String getApplicant() {
+        return applicant;
+    }
+
+    public void setApplicant(String applicant) {
+        this.applicant = applicant;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "TrademarkInfo{" +
+                "trademarkPic='" + trademarkPic + '\'' +
+                ", name='" + name + '\'' +
+                ", regNo='" + regNo + '\'' +
+                ", classify='" + classify + '\'' +
+                ", applyTime=" + applyTime +
+                ", applicant='" + applicant + '\'' +
+                ", type='" + type + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
