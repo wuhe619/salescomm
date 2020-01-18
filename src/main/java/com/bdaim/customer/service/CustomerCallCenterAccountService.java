ea83a3890a3c84e79e89e22bbb05409fbd3ccad8
chengning <chengning@bdaim.com>
2020-01-18 15:10:13 +0800

    fix:客户sql注入修改

--- src/main/java/com/bdaim/customer/service/CustomerCallCenterAccountService.java
+++ src/main/java/com/bdaim/customer/service/CustomerCallCenterAccountService.java
@@ -17,12 +17,14 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 
 /**
+ *
  */
 @Service("customerCallCenterAccountService")
 @Transactional
@@ -37,6 +39,7 @@ public class CustomerCallCenterAccountService {
 
     /**
      * 分页查询
+     *
      * @param customerName
      * @param customerUserName
      * @param callCenterId
@@ -44,34 +47,39 @@ public class CustomerCallCenterAccountService {
      * @param pageSize
      * @return
      */
-    public JSONObject page(String customerName, String customerUserName, String callCenterId, Integer pageNum, Integer pageSize){
+    public JSONObject page(String customerName, String customerUserName, String callCenterId, Integer pageNum, Integer pageSize) {
         JSONObject jsonObject = new JSONObject();
-        String sql=" select a.*,b.enterprise_name,c.account from t_customer_callcenter a left join t_customer b on a.cust_id=b.cust_id left join t_customer_user c on a.cust_id=c.cust_id and c.user_type=1 where 1=1 ";
-        if(StringUtil.isNotEmpty(customerName)){
-            sql+= " and b.enterprise_name = '"+customerName+"'";
+        String sql = " select a.*,b.enterprise_name,c.account from t_customer_callcenter a left join t_customer b on a.cust_id=b.cust_id left join t_customer_user c on a.cust_id=c.cust_id and c.user_type=1 where 1=1 ";
+        List<Object> p = new ArrayList<>();
+        if (StringUtil.isNotEmpty(customerName)) {
+            p.add(customerName);
+            sql += " and b.enterprise_name = ? ";
         }
-        if(StringUtil.isNotEmpty(customerUserName)){
-            sql+=" and c.account='"+customerUserName+"'";
+        if (StringUtil.isNotEmpty(customerUserName)) {
+            p.add(customerUserName);
+            sql += " and c.account=? ";
         }
-        if(StringUtil.isNotEmpty(callCenterId)){
-            sql+= " and a.id = "+callCenterId;
+        if (StringUtil.isNotEmpty(callCenterId)) {
+            p.add(callCenterId);
+            sql += " and a.id = ? ";
         }
-        sql+=" order by a.create_time desc ";
-        String countSql="select count(0) from ("+sql+")a";
-        String total = customerCallCenterAccountDao.queryForObject(countSql);
-        if(StringUtil.isNotEmpty(total) && Integer.valueOf(total)>0){
-            sql+=" limit "+pageNum+","+pageSize;
-        }else{
-            jsonObject.put("total",0);
+        sql += " order by a.create_time desc ";
+        String countSql = "select count(0) from (" + sql + ")a";
+        String total = customerCallCenterAccountDao.queryForObject(countSql, p.toArray());
+        if (StringUtil.isNotEmpty(total) && Integer.valueOf(total) > 0) {
+            sql += " limit " + pageNum + "," + pageSize;
+        } else {
+            jsonObject.put("total", 0);
             return jsonObject;
         }
-        List<Map<String, Object>> accounts = customerCallCenterAccountDao.queryListBySql(sql);
-        jsonObject.put("data",accounts);
+        List<Map<String, Object>> accounts = customerCallCenterAccountDao.sqlQuery(sql, p.toArray());
+        jsonObject.put("data", accounts);
         return jsonObject;
     }
 
     /**
      * 保存/编辑
+     *
      * @param customerCallCenterAccount
      * @param lu
      * @throws TouchException
@@ -80,16 +88,16 @@ public class CustomerCallCenterAccountService {
 
         if (customerCallCenterAccount.getId() != null) {
             CustomerCallCenterAccount dbobj = customerCallCenterAccountDao.get(customerCallCenterAccount.getId());
-            if(dbobj==null || dbobj.getStatus()==2){
+            if (dbobj == null || dbobj.getStatus() == 2) {
                 throw new TouchException("数据不存在或已删除");
             }
-            if(customerCallCenterAccount.getAuthorizedSeats()!=null){
+            if (customerCallCenterAccount.getAuthorizedSeats() != null) {
                 dbobj.setAuthorizedSeats(customerCallCenterAccount.getAuthorizedSeats());
             }
-            if(customerCallCenterAccount.getMaxOccurs()!=null){
+            if (customerCallCenterAccount.getMaxOccurs() != null) {
                 dbobj.setMaxOccurs(customerCallCenterAccount.getMaxOccurs());
             }
-            if(customerCallCenterAccount.getStatus()!=null){
+            if (customerCallCenterAccount.getStatus() != null) {
                 dbobj.setStatus(customerCallCenterAccount.getStatus());
             }
             dbobj.setLastUpdateUser(lu.getId().toString());
@@ -97,16 +105,16 @@ public class CustomerCallCenterAccountService {
 
             XzCompanyCallcenterParam param = openAccountRequestParam(dbobj);
             JSONObject json = XzCallCenterUtil.updateCompanytoXzCallCenter(param);
-            logger.info("updateCompanytoXzCallCenter.json:"+json.toJSONString());
+            logger.info("updateCompanytoXzCallCenter.json:" + json.toJSONString());
             if (json != null && "0".equals(json.getString("code"))) {
                 customerCallCenterAccountDao.save(dbobj);
-            }else{
+            } else {
                 logger.info("updateCompanytoXzCallCenter failed ");
             }
         } else {
-            List<CustomerCallCenterAccount> list = customerCallCenterAccountDao.findBy("custId",customerCallCenterAccount.getCustId());
-            if(list!=null && list.size()>0){
-                throw new TouchException("企业 "+customerCallCenterAccount.getCustomerName()+" 已开通过账号");
+            List<CustomerCallCenterAccount> list = customerCallCenterAccountDao.findBy("custId", customerCallCenterAccount.getCustId());
+            if (list != null && list.size() > 0) {
+                throw new TouchException("企业 " + customerCallCenterAccount.getCustomerName() + " 已开通过账号");
             }
             Date d = new Date();
             customerCallCenterAccount.setCreateTime(d);
@@ -127,8 +135,8 @@ public class CustomerCallCenterAccountService {
                     logger.info("addCompanytoXzCallCenter failed");
                     customerCallCenterAccountDao.delete(customerCallCenterAccount.getId());
                 }
-            }catch (Exception e){
-                logger.error("addCompanytoXzCallCenter error : "+e.getMessage());
+            } catch (Exception e) {
+                logger.error("addCompanytoXzCallCenter error : " + e.getMessage());
                 customerCallCenterAccountDao.delete(customerCallCenterAccount.getId());
             }
         }
@@ -137,14 +145,15 @@ public class CustomerCallCenterAccountService {
 
     /**
      * 拼装参数
+     *
      * @param dbobj
      * @return
      */
-    private XzCompanyCallcenterParam openAccountRequestParam(CustomerCallCenterAccount dbobj){
+    private XzCompanyCallcenterParam openAccountRequestParam(CustomerCallCenterAccount dbobj) {
         XzCompanyCallcenterParam param = new XzCompanyCallcenterParam();
         param.setAmountagentauth(dbobj.getAuthorizedSeats());
-        param.setBegintime(DateUtil.fmtDateToStr(dbobj.getStartTime(),"yyyy-MM-dd"));
-        param.setEndtime(DateUtil.fmtDateToStr(dbobj.getEndTime(),"yyyy-MM-dd"));
+        param.setBegintime(DateUtil.fmtDateToStr(dbobj.getStartTime(), "yyyy-MM-dd"));
+        param.setEndtime(DateUtil.fmtDateToStr(dbobj.getEndTime(), "yyyy-MM-dd"));
         Customer customer = customerDao.get(dbobj.getCustId());
         param.setCompanyname(customer.getEnterpriseName());
         param.setCompid(dbobj.getId().toString());
@@ -155,10 +164,11 @@ public class CustomerCallCenterAccountService {
 
     /**
      * 返回详情
+     *
      * @param id
      * @return
      */
-    public CustomerCallCenterAccount getDetailById(Long id){
+    public CustomerCallCenterAccount getDetailById(Long id) {
         return customerCallCenterAccountDao.get(id);
     }
 }
