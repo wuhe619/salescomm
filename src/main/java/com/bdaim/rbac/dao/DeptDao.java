50786916e4b7035adddc8ad3f1b77188ce599315
chengning <chengning@bdaim.com>
2020-01-19 00:57:28 +0800

    fix:部门防止sql注入

--- src/main/java/com/bdaim/rbac/dao/DeptDao.java
+++ src/main/java/com/bdaim/rbac/dao/DeptDao.java
@@ -72,7 +72,7 @@ public class DeptDao extends SimpleHibernateDao<DeptEntity, Serializable> {
     }
 
     public void insert(DeptDTO t) throws SQLException {
-        this.executeUpdateSQL("insert into t_dept(ID,NAME,OPTUSER,CREATE_TIME,TYPE) values(" + t.getId() + ",'" + t.getName() + "','" + t.getOptuser() + "',now(),'" + t.getType() + "')");
+        this.executeUpdateSQL("insert into t_dept(ID,NAME,OPTUSER,CREATE_TIME,TYPE) values(?,?,?,now(),?)", t.getId(), t.getName(), t.getOptuser(), t.getType());
     }
 
     public void delete(DeptDTO t) {
@@ -86,15 +86,15 @@ public class DeptDao extends SimpleHibernateDao<DeptEntity, Serializable> {
         sb.append("update t_dept set MODIFY_TIME=now(),");
         List<Object> p = new ArrayList<>();
         if (t.getOptuser() != null && !t.getOptuser().equals("")) {
-            sb.append("OPTUSER=? ");
+            sb.append(" OPTUSER=?, ");
             p.add(t.getOptuser());
         }
         if (t.getName() != null && !t.getName().equals("")) {
-            sb.append("NAME=? ");
+            sb.append(" NAME=? ");
             p.add(t.getName());
         }
         //确认SQL，绑定参数
-        this.executeUpdateSQL(sb.substring(0, sb.length() - 1) + " where ID=?", t.getId(), p.toArray());
+        this.executeUpdateSQL(sb.substring(0, sb.length() - 1) + " where ID=? ", t.getId(), p.toArray());
     }
 
     public DeptDTO getObj(Connection con, DeptDTO t) {
