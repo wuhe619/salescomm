f244bc8ac6eedcb12d770fdf310da3f7e0d7513f
ningmeng0218 <ningcheng@cloudinnov.com>
2020-02-14 15:11:53 +0800

    feat:线索公海返回企业ID

--- src/main/java/com/bdaim/crm/dao/LkCrmLeadsDao.java
+++ src/main/java/com/bdaim/crm/dao/LkCrmLeadsDao.java
@@ -60,7 +60,7 @@ public class LkCrmLeadsDao extends SimpleHibernateDao<LkCrmLeadsEntity, Integer>
     }
 
     public Page pageCluePublicSea(int pageNum, int pageSize, long seaId, String search) {
-        StringBuffer conditions = new StringBuffer("SELECT custG.id, custG.user_id, custG.status, custG.call_count callCount, DATE_FORMAT(custG.last_call_time,'%Y-%m-%d %H:%i:%s') lastCallTime, custG.intent_level intentLevel,");
+        StringBuffer conditions = new StringBuffer("SELECT custG.super_data ->> '$.SYS014' id, custG.user_id, custG.status, custG.call_count callCount, DATE_FORMAT(custG.last_call_time,'%Y-%m-%d %H:%i:%s') lastCallTime, custG.intent_level intentLevel,");
         conditions.append(" custG.super_name, custG.super_age, custG.super_sex, custG.super_telphone, custG.super_phone, custG.super_address_province_city, custG.super_address_street, custG.super_data, ");
         conditions.append(" custG.batch_id, custG.last_call_status, custG.data_source, DATE_FORMAT(custG.user_get_time,'%Y-%m-%d %H:%i:%s') user_get_time, DATE_FORMAT(custG.create_time,'%Y-%m-%d %H:%i:%s') create_time, custG.pre_user_id, custG.last_called_duration, DATE_FORMAT(custG.last_mark_time,'%Y-%m-%d %H:%i:%s') last_mark_time, ");
         conditions.append(" custG.call_success_count, custG.call_fail_count, custG.sms_success_count ,custG.super_data ->> '$.SYS014 ' as custType, ");
