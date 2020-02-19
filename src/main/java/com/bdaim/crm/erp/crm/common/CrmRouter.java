5f800667288328cbdfbf64cfc480b47267536719
ningmeng0218 <ningcheng@cloudinnov.com>
2020-02-19 15:01:36 +0800

    feat:CrmReceivablesController

--- src/main/java/com/bdaim/crm/erp/crm/common/CrmRouter.java
+++ src/main/java/com/bdaim/crm/erp/crm/common/CrmRouter.java
@@ -7,14 +7,14 @@ public class CrmRouter extends Routes {
     @Override
     public void config() {
         addInterceptor(new CrmInterceptor());
-        add("/CrmProduct", CrmProductController.class);
-        add("/CrmProductCategory", CrmProductCategoryController.class);
+        //add("/CrmProduct", CrmProductController.class);
+        //add("/CrmProductCategory", CrmProductCategoryController.class);
         //add("/CrmLeads", CrmLeadsController.class);
-        add("/CrmCustomer", CrmCustomerController.class);
-        add("/CrmBusiness", CrmBusinessController.class);
-        add("/CrmContacts", CrmContactsController.class);
-        add("/CrmContract", CrmContractController.class);
-        add("/CrmReceivables", CrmReceivablesController.class);
+        //add("/CrmCustomer", CrmCustomerController.class);
+        //add("/CrmBusiness", CrmBusinessController.class);
+        //add("/CrmContacts", CrmContactsController.class);
+        //add("/CrmContract", CrmContractController.class);
+        //add("/CrmReceivables", CrmReceivablesController.class);
         add("/CrmRecord",CrmRecordController.class);
         add("/Crm/ReceivablesPlan",CrmReceivablesPlanController.class);
         add("/Crm/Instrument",InstrumentController.class);
