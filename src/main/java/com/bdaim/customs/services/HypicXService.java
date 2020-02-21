cf877eb8d50a74c885590cd1d5bac54cc3e226e5
wuhe <wuhe@bdaim.com>
2020-02-21 14:47:07 +0800

    api调用记录

--- src/main/java/com/bdaim/customs/services/HypicXService.java
+++ src/main/java/com/bdaim/customs/services/HypicXService.java
@@ -176,9 +176,10 @@ public class HypicXService implements BusiService {
     @Override
     public String formatQuery(String busiType, String cust_id, String cust_group_id, Long cust_user_id, JSONObject
             params, List sqlParams) {
-        String _orderby_ = params.getString("_orderby_");
-        String _sort_ = params.getString("_sort_");
+        //String _orderby_ = params.getString("_orderby_");
+//        String _sort_ = params.getString("_sort_");
         StringBuffer sqlstr = new StringBuffer("select id, content , cust_id, create_id, create_date,update_date,ext_1, ext_2 , ext_3, ext_4, ext_5 from " + HMetaDataDef.getTable(busiType, "") + " where type=?");
+        sqlParams.add(busiType);
         if (!"all".equals(cust_id)) {
             sqlstr.append(" and cust_id=? ");
             sqlParams.add(cust_id);
