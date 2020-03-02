7a61940ea27e12f492bb6cfa36986e61cfb87f9c
zhangbf <10369446@qq.com>
2020-03-02 16:36:12 +0800

    refactor:  bi销售漏斗分析 相关返回值格式重构

--- src/main/java/com/bdaim/crm/erp/bi/controller/BiFunnelController.java
+++ src/main/java/com/bdaim/crm/erp/bi/controller/BiFunnelController.java
@@ -1,45 +1,66 @@
 package com.bdaim.crm.erp.bi.controller;
 
+import com.bdaim.crm.utils.R;
 import com.jfinal.aop.Inject;
 import com.jfinal.core.Controller;
 import com.jfinal.core.paragetter.Para;
 import com.bdaim.crm.erp.bi.service.BiFunnelService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+@RestController
+@RequestMapping(value = "/biFunnel")
 public class BiFunnelController extends Controller {
     @Resource
     private BiFunnelService service;
+
     /**
      * 销售漏斗
+     *
      * @author zxy
      */
-    public void sellFunnel(@Para("deptId") Integer deptId, @Para("userId") Long userId, @Para("type") String type,
-                           @Para("startTime") String startTime, @Para("endTime") String endTime, @Para("typeId") Integer typeId){
-        renderJson(service.sellFunnel(deptId,userId,type,startTime,endTime,typeId));
+    @RequestMapping(value = "/sellFunnel")
+    public R sellFunnel(@Para("deptId") Integer deptId, @Para("userId") Long userId, @Para("type") String type,
+                        @Para("startTime") String startTime, @Para("endTime") String endTime, @Para("typeId") Integer typeId) {
+//        renderJson(service.sellFunnel(deptId,userId,type,startTime,endTime,typeId));
+        return service.sellFunnel(deptId, userId, type, startTime, endTime, typeId);
     }
+
     /**
      * 新增商机分析图
+     *
      * @author zxy
      */
-    public void addBusinessAnalyze(@Para("deptId") Integer deptId, @Para("userId") Long userId, @Para("type") String type,
-                                   @Para("startTime") String startTime, @Para("endTime") String endTime){
-        renderJson(service.addBusinessAnalyze(deptId,userId,type,startTime,endTime));
+    @RequestMapping(value = "/addBusinessAnalyze")
+    public R addBusinessAnalyze(@Para("deptId") Integer deptId, @Para("userId") Long userId, @Para("type") String type,
+                                @Para("startTime") String startTime, @Para("endTime") String endTime) {
+//        renderJson(service.addBusinessAnalyze(deptId,userId,type,startTime,endTime));
+        return service.addBusinessAnalyze(deptId, userId, type, startTime, endTime);
     }
+
     /**
      * 新增商机分析表
+     *
      * @author zxy
      */
-    public void sellFunnelList(@Para("deptId") Integer deptId, @Para("userId") Long userId, @Para("type") String type,
-                               @Para("startTime") String startTime, @Para("endTime") String endTime){
-        renderJson(service.sellFunnelList(deptId,userId,type,startTime,endTime));
+    @RequestMapping(value = "/sellFunnelList")
+    public R sellFunnelList(@Para("deptId") Integer deptId, @Para("userId") Long userId, @Para("type") String type,
+                            @Para("startTime") String startTime, @Para("endTime") String endTime) {
+//        renderJson(service.sellFunnelList(deptId,userId,type,startTime,endTime));
+        return service.sellFunnelList(deptId, userId, type, startTime, endTime);
     }
+
     /**
      * 商机转化率分析
+     *
      * @author zxy
      */
-    public void win(@Para("deptId") Integer deptId, @Para("userId") Long userId, @Para("type") String type,
-                    @Para("startTime") String startTime, @Para("endTime") String endTime){
-        renderJson(service.win(deptId,userId,type,startTime,endTime));
+    @RequestMapping(value = "/win")
+    public R win(@Para("deptId") Integer deptId, @Para("userId") Long userId, @Para("type") String type,
+                 @Para("startTime") String startTime, @Para("endTime") String endTime) {
+//        renderJson(service.win(deptId,userId,type,startTime,endTime));
+        return service.win(deptId, userId, type, startTime, endTime);
     }
 }
