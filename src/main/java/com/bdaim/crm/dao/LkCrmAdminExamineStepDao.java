77699d48823aeba6bfb8379472d63c1e748f06bb
ningmeng0218 <ningcheng@cloudinnov.com>
2020-03-04 10:10:36 +0800

    feat:AdminExamineController审批流程

--- src/main/java/com/bdaim/crm/dao/LkCrmAdminExamineStepDao.java
+++ src/main/java/com/bdaim/crm/dao/LkCrmAdminExamineStepDao.java
@@ -19,8 +19,8 @@ public class LkCrmAdminExamineStepDao extends SimpleHibernateDao<LkCrmAdminExami
     }
 
     public LkCrmAdminExamineStepEntity queryExamineStepByExamineId(Integer examineId) {
-        String sql = "  select * from LkCrmAdminExamineStepEntity where  examineId = ?";
-        List<LkCrmAdminExamineStepEntity> objects = find(sql, examineId);
+        String hql = "from LkCrmAdminExamineStepEntity where examineId = ?";
+        List<LkCrmAdminExamineStepEntity> objects = find(hql, examineId);
         if (objects.size() > 0) {
             return objects.get(0);
         }
