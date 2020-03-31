fddb65543c03657f56b613c7acae7bd8b9c73abb
chengning <chengning@bdaim.com>
2020-03-31 16:35:48 +0800

    feat:企业数据经营状态检索

--- src/main/java/com/bdaim/crm/ent/service/EntDataService.java
+++ src/main/java/com/bdaim/crm/ent/service/EntDataService.java
@@ -773,12 +773,9 @@ public class EntDataService {
             JSONArray jsonArray = param.getJSONArray("regStatus");
             BoolQueryBuilder temp = QueryBuilders.boolQuery();
             for (int i = 0; i < jsonArray.size(); i++) {
-                JSONArray texts = jsonArray.getJSONObject(i).getJSONArray("value");
-                for (int j = 0; j < texts.size(); j++) {
-                    WildcardQueryBuilder mpq = QueryBuilders
-                            .wildcardQuery("entStatus","*" + texts.getString(j) + "*");
-                    temp.should(mpq);
-                }
+                WildcardQueryBuilder mpq = QueryBuilders
+                        .wildcardQuery("entStatus", "*" + jsonArray.getJSONObject(i).getString("value") + "*");
+                temp.should(mpq);
             }
             qb.must(temp);
         }
