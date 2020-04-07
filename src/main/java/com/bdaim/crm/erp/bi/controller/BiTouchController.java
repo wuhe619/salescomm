00e73341966239fb9415da4566aa9b00fe8946ba
zhangbf <10369446@qq.com>
2020-04-07 11:36:08 +0800

    feat:电话短信统计分析 1

--- src/main/java/com/bdaim/crm/erp/bi/controller/BiTouchController.java
+++ src/main/java/com/bdaim/crm/erp/bi/controller/BiTouchController.java
@@ -1,12 +1,10 @@
 package com.bdaim.crm.erp.bi.controller;
 
-import com.bdaim.common.dto.PageParam;
 import com.bdaim.common.response.ResponseInfo;
 import com.bdaim.common.response.ResponseInfoAssemble;
 import com.bdaim.crm.erp.bi.service.BiTouchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,9 +21,9 @@ public class BiTouchController {
      * @author Chacker
      */
     @RequestMapping(value = "/phone")
-    public ResponseInfo phone(@RequestParam Integer deptId, @RequestParam Long userId,
-                              @RequestParam String type) {
-        return biTouchService.phone(deptId, userId, type);
+    public ResponseInfo phone(Integer deptId, Long userId, String type,
+                              String startTime, String endTime) {
+        return biTouchService.phone(deptId, userId, type, startTime, endTime);
     }
 
     /**
@@ -38,9 +36,9 @@ public class BiTouchController {
      * @author Chacker
      */
     @RequestMapping(value = "/textMessage")
-    public ResponseInfo textMessage(@RequestParam Integer deptId, @RequestParam Long userId,
-                                    @RequestParam String type) {
-        return biTouchService.textMessage(deptId, userId, type);
+    public ResponseInfo textMessage(Integer deptId, Long userId,
+                                    String type, String startTime, String endTime) {
+        return biTouchService.textMessage(deptId, userId, type, startTime, endTime);
     }
 
     /**
@@ -54,13 +52,13 @@ public class BiTouchController {
      * @return
      */
     @RequestMapping(value = "/phoneList")
-    public ResponseInfo phoneList(@RequestParam Integer pageNum, @RequestParam Integer pageSize,
-                                  @RequestParam Integer deptId,
-                                  @RequestParam Long userId, @RequestParam String type) {
+    public ResponseInfo phoneList(Integer pageNum, Integer pageSize,
+                                  Integer deptId, Long userId, String type,
+                                  String startTime, String endTime) {
         if (pageNum == null || pageSize == null) {
             return new ResponseInfoAssemble().failure(500, "请选择页码");
         }
-        return biTouchService.phoneList(pageNum, pageSize, deptId, userId, type);
+        return biTouchService.phoneList(pageNum, pageSize, deptId, userId, type, startTime, endTime);
     }
 
     /**
@@ -74,13 +72,13 @@ public class BiTouchController {
      * @return
      */
     @RequestMapping(value = "/messageList")
-    public ResponseInfo messageList(@RequestParam Integer pageNum, @RequestParam Integer pageSize,
-                                    @RequestParam Integer deptId,
-                                    @RequestParam Long userId, @RequestParam String type) {
+    public ResponseInfo messageList(Integer pageNum, Integer pageSize,
+                                    Integer deptId, Long userId, String type,
+                                    String startTime, String endTime) {
         if (pageNum == null || pageSize == null) {
             return new ResponseInfoAssemble().failure(500, "请选择页码");
         }
-        return biTouchService.messageList(pageNum, pageSize, deptId, userId, type);
+        return biTouchService.messageList(pageNum, pageSize, deptId, userId, type, startTime, endTime);
 
     }
 }
