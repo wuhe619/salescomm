9e30c87cf9df503f9af7ecea0fe447b2b3e9e8eb
chengning <chengning@bdaim.com>
2020-04-08 20:38:03 +0800

    feat:CRM退回公海异步

--- src/main/java/com/bdaim/crm/erp/crm/service/CrmLeadsService.java
+++ src/main/java/com/bdaim/crm/erp/crm/service/CrmLeadsService.java
@@ -50,6 +50,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -715,7 +716,8 @@ public class CrmLeadsService {
      * @param batchId
      * @return
      */
-    public int transferToPublicSea(String seaId, String userId, String batchId, LoginUser user) {
+    @Async
+    public int transferToPublicSea(String seaId, String userId, String batchId, LoginUser user, String leadsview) {
         //添加到线索私海数据
         StringBuilder sql = new StringBuilder()
                 .append("SELECT * FROM lkcrm_crm_leads  WHERE batch_id =? ");
@@ -723,7 +725,7 @@ public class CrmLeadsService {
         int i = 0;
         String insertSql = "REPLACE INTO " + ConstantsUtil.SEA_TABLE_PREFIX + seaId + " (`id`, `user_id`, `update_time`, `status`, " +
                 "super_name,super_telphone,super_phone,super_data,create_time,batch_id) VALUES(?,?,?,?,?,?,?,?,?,?)";
-        String leadsview = BaseUtil.getViewSql("leadsview");
+
         for (Map<String, Object> m : maps) {
             JSONObject superData = new JSONObject();
             superData.put("SYS005", m.get("company"));
@@ -735,6 +737,9 @@ public class CrmLeadsService {
                     , m.get("mobile"), m.get("telephone"), superData.toJSONString(), m.get("create_time"), csp.getPropertyValue());
             // 退回到公海线索
             List<Map<String, Object>> list = crmLeadsDao.sqlQuery("select * from " + leadsview + " where batch_id = ?", batchId);
+            if (list == null || list.size() == 0) {
+                continue;
+            }
             Record crmLeads = JavaBeanUtil.mapToRecord(list.get(0));
             List<Record> leadsFields = adminFieldService.list("1");
             List<LkCrmAdminFieldEntity> seaFields = crmLeadsDao.find("from LkCrmAdminFieldEntity where label = '11' AND custId = ? ", user.getCustId());
@@ -1292,6 +1297,7 @@ public class CrmLeadsService {
     /**
      * 根据id 删除线索
      */
+    @Async
     public R deleteByBatchIds(List idsList) {
         if (idsList == null || idsList.size() == 0) {
             R.error("leadsIds不能为空");
@@ -1342,10 +1348,11 @@ public class CrmLeadsService {
         customerSeaDao.executeUpdateSQL(logSql.toString(), p.toArray());
         int status = customerSeaDao.executeUpdateSQL(sql.toString(), param.toArray());
         LoginUser user = BaseUtil.getUser();
+        String leadsview = BaseUtil.getViewSql("leadsview");
         for (String id : superIds) {
             List<Map<String, Object>> list = customerSeaDao.sqlQuery("select * from " + ConstantsUtil.SEA_TABLE_PREFIX + seaId + " WHERE id = ? ", id);
             if (list.size() == 0) {
-                transferToPublicSea(seaId, userId.toString(), id, user);
+                transferToPublicSea(seaId, userId.toString(), id, user, leadsview);
             }
         }
         return status;
