ad0bb3fd5aef71af04bb1462e6925c85695558b4
chengning <chengning@bdaim.com>
2020-04-13 10:11:25 +0800

    feat:打电话判断余额

---
+++ src/main/java/com/bdaim/crm/erp/crm/controller/CrmCommonController.java
@@ -0,0 +1,45 @@
+package com.bdaim.crm.erp.crm.controller;
+
+import com.bdaim.auth.LoginUser;
+import com.bdaim.common.controller.BasicAction;
+import com.bdaim.common.controller.util.ResponseCommon;
+import com.bdaim.common.exception.TouchException;
+import com.bdaim.crm.erp.crm.service.CrmCommonService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chengning@salescomm.net
+ * @description 工具controller
+ * @date 2020/4/13 9:43
+ */
+@RestController
+@RequestMapping("/crm/common")
+public class CrmCommonController extends BasicAction {
+
+    public static final Logger LOG = LoggerFactory.getLogger(CrmCommonController.class);
+
+    @Autowired
+    private CrmCommonService crmCommonService;
+
+    @PostMapping(value = "/call/validSeat", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseCommon validSeat() {
+        LoginUser lu = opUser();
+        ResponseCommon response = new ResponseCommon();
+        boolean result;
+        try {
+            result = crmCommonService.isValidAccount(lu);
+        } catch (TouchException e) {
+            LOG.error("CRM判断用户方能否致电发短信异常,", e);
+            response.setMessage(e.getMessage());
+            result = false;
+        }
+        response.setCode(result ? 1 : -1);
+        return response;
+    }
+}
---
+++ src/main/java/com/bdaim/crm/erp/crm/service/CrmCommonService.java
@@ -0,0 +1,44 @@
+package com.bdaim.crm.erp.crm.service;
+
+import com.bdaim.auth.LoginUser;
+import com.bdaim.common.exception.TouchException;
+import com.bdaim.customer.dao.CustomerUserDao;
+import com.bdaim.customer.entity.CustomerUserPropertyDO;
+import com.bdaim.resource.service.MarketResourceService;
+import com.bdaim.util.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author chengning@salescomm.net
+ * @description TODO
+ * @date 2020/4/13 9:45
+ */
+@Service
+@Transactional
+public class CrmCommonService {
+
+    @Autowired
+    private MarketResourceService marketResourceService;
+    @Autowired
+    private CustomerUserDao customerUserDao;
+
+    /**
+     * 判断用户能否致电
+     * 1.判断余额
+     */
+    public Boolean isValidAccount(LoginUser lu) throws TouchException {
+        boolean has_remain = marketResourceService.judRemainAmount(lu.getCustId());
+        if (!has_remain && "2".equals(lu.getUserType())) {
+            throw new TouchException("余额不足");
+        }
+        if ("2".equals(lu.getUserType())) {
+            CustomerUserPropertyDO call_channel = customerUserDao.getProperty(lu.getId().toString(), "call_channel");
+            if (call_channel == null || StringUtil.isEmpty(call_channel.getPropertyValue())) {
+                throw new TouchException("坐席未配置外呼参数");
+            }
+        }
+        return true;
+    }
+}
