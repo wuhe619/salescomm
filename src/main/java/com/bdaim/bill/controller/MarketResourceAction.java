0782fc6106f9619c8e381460536ecd0dc2c4a592
chengning <chengning@bdaim.com>
2020-05-13 11:29:50 +0800

    feat:讯众自动外呼回调参数打印

--- src/main/java/com/bdaim/bill/controller/MarketResourceAction.java
+++ src/main/java/com/bdaim/bill/controller/MarketResourceAction.java
@@ -3968,9 +3968,14 @@ public class MarketResourceAction extends BasicAction {
     @RequestMapping(value = "/xzautolog", method = RequestMethod.POST)
     @CacheAnnotation
     public String xzAutoLog(@RequestParam Map param) {
-        LoginUser lu = opUser();
-        LOG.info("用户ID:{},请求参数:{}", lu.getId(), param);
-        return "success";
+        try {
+            LoginUser lu = opUser();
+            LOG.warn("讯众自动外呼回调参数打印用户ID:{},请求参数:{}", lu.getId(), param);
+        } catch (Exception e) {
+            LOG.error("讯众自动外呼回调参数打印异常", e);
+        } finally {
+            return "success";
+        }
     }
 
 }
