225e71bdcbbe3ded3967165b44997055ffed7eef
chengning <chengning@bdaim.com>
2020-06-04 20:30:23 +0800

    feat:#

--- src/main/java/com/bdaim/AppConfig.java
+++ src/main/java/com/bdaim/AppConfig.java
@@ -274,11 +274,11 @@ public class AppConfig {
 	}
 
 	public static String getEnt_data_index() {
-		return ent_data_index;
+		return "retrieve";
 	}
-	@Value("${ent_data_index:ent_data_test}")
+
 	public void setEnt_data_index(String ent_data_index) {
-		AppConfig.ent_data_index = ent_data_index;
+		AppConfig.ent_data_index = "retrieve";
 	}
 
 	public static String getEnt_data_type() {
