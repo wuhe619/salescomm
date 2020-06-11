d26e89623335f3773e6f27d9be7d418a31776266
chengning <chengning@bdaim.com>
2020-06-11 17:30:49 +0800

    feat:CRM代办事项即将到期的客户

--- src/main/java/com/bdaim/crm/dao/LkCrmCustomerDao.java
+++ src/main/java/com/bdaim/crm/dao/LkCrmCustomerDao.java
@@ -83,7 +83,8 @@ public class LkCrmCustomerDao extends SimpleHibernateDao<LkCrmCustomerEntity, In
     public int endCustomerNum(int followupDay, int dealDay, String userId) {
         String sql = "SELECT COUNT(0) FROM lkcrm_crm_customer AS ccc WHERE owner_user_id != 0 AND deal_status = '未成交' AND is_lock = 0 " +
                 " AND( ( to_days(now()) - to_days( IFNULL( ( SELECT car.create_time FROM lkcrm_admin_record AS car WHERE car.types = 'crm_customer' " +
-                " AND car.types_id = ccc.customer_id ORDER BY car.create_time DESC LIMIT 1), ccc.create_time ) ) ) >= abs(?) OR ( to_days(now()) - to_days(create_time) ) >= abs(?) ) " +
+                " AND car.types_id = ccc.customer_id ORDER BY car.create_time DESC LIMIT 1), ccc.create_time ) ) ) >= abs(?) " +
+                " OR (( to_days(now()) - to_days(create_time) ) >= abs(?) AND ( (SELECT count(customer_id) FROM lkcrm_crm_business WHERE customer_id = ccc.customer_id  )=0 AND (SELECT count(customer_id)  FROM lkcrm_crm_contract WHERE customer_id = ccc.customer_id  )=0) )) " +
                 " AND ccc.owner_user_id = ?";
         return queryForInt(sql, followupDay, dealDay, userId);
     }
